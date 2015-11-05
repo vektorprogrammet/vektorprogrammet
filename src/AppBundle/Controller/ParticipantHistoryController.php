d11179720b5c37da1879b9d81ee6bb6ae39f6e49
aestene <arnterik1994@hotmail.com>
2015-11-05 20:50:26 +0100

    Sortering og visning av deltakerhistorie semester/avdeling

--- src/AppBundle/Controller/ParticipantHistoryController.php
+++ src/AppBundle/Controller/ParticipantHistoryController.php
@@ -3,10 +3,11 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class ParticipantHistoryController extends Controller
 {
-    public function showAction() {
+    public function showAction(Request $request) {
 		
 		// Only Team members or above can see this page
 		if ($this->get('security.context')->isGranted('ROLE_ADMIN')) {
@@ -19,11 +20,23 @@ class ParticipantHistoryController extends Controller
 			
 			// Find all departments
 			$departments = $this->getDoctrine()->getRepository('AppBundle:Department')->findAll();
+
+			// Finds the department for the current logged in user
+			$department= $this->get('security.token_storage')->getToken()->getUser()->getFieldOfStudy()->getDepartment();
+
+			$semesters =  $this->getDoctrine()->getRepository('AppBundle:Semester')->findAllSemestersByDepartment($department);
+
+			//Finds the semester to sort by
+			$sortBySemester = $request->query->get('semester', null);
+
 			
 			return $this->render('participant_history/index.html.twig', array(
 				'workHistories' => $workHistories,
 				'assistantHistories' => $assistantHistories,
-				'departments' => $departments, 
+				'departments' => $departments,
+				'department' => $department,
+				'semesters' => $semesters,
+				'sortBySemester' => $sortBySemester,
 			));
 		}
 		else {
@@ -31,4 +44,41 @@ class ParticipantHistoryController extends Controller
 		}
 		
     }
+
+	public function showHistoriesByDepartmentAction(Request $request, $id) {
+
+		// Only Team members or above can see this page
+		if ($this->get('security.context')->isGranted('ROLE_ADMIN')) {
+
+			// Find all work histories by department
+			$workHistories = $this->getDoctrine()->getRepository('AppBundle:WorkHistory')->findWorkHistoriesByDepartment($id);
+			// Find all assistantHistories by department
+			$assistantHistories = $this->getDoctrine()->getRepository('AppBundle:AssistantHistory')->findAssistantHistoriesByDepartment($id);
+
+			// Find all departments
+			$departments = $this->getDoctrine()->getRepository('AppBundle:Department')->findAll();
+
+			// Finds the department we're currently testing for
+			$department = $this->getDoctrine()->getRepository('AppBundle:Department')->findDepartmentById($id)[0];
+
+			$semesters =  $this->getDoctrine()->getRepository('AppBundle:Semester')->findAllSemestersByDepartment($id);
+
+			//Finds the semester to sort by
+			$sortBySemester = $request->query->get('semester', null);
+
+
+			return $this->render('participant_history/index.html.twig', array(
+				'workHistories' => $workHistories,
+				'assistantHistories' => $assistantHistories,
+				'departments' => $departments,
+				'department' => $department,
+				'semesters' => $semesters,
+				'sortBySemester' => $sortBySemester,
+			));
+		}
+		else {
+			return $this->redirect($this->generateUrl('home'));
+		}
+
+	}
 }
--- src/AppBundle/Entity/Repository/AssistantHistoryRepository.php
+++ src/AppBundle/Entity/Repository/AssistantHistoryRepository.php
@@ -87,6 +87,43 @@ class AssistantHistoryRepository extends EntityRepository {
 
 		return $assistantHistories;
 	}
-	
+	/*
+	public function findActiveAssistantHistoriesByDepartment($department){
+
+		$today = new \DateTime('now');
+		$assistantHistories =  $this->getEntityManager()->createQuery("
+
+		SELECT ahistory
+		FROM AppBundle:AssistantHistory ahistory
+		JOIN ahistory.semester.department department
+		JOIN ahistory.semester semester
+		JOIN ahistory.user user
+		WHERE ahistory.semester.department = :department
+		AND semester.semesterStartDate < :today
+		AND semester.semesterEndDate > :today
+		")
+			->setParameter('department', $department)
+			->setParameter('today', $today)
+			->getResult();
+
+		return $assistantHistories;
+	}
+	*/
+
+	public function findAssistantHistoriesByDepartment($department){
+		$today = new \DateTime('now');
+		$assistantHistories =  $this->getEntityManager()->createQuery("
+		SELECT ahistory
+		FROM AppBundle:AssistantHistory ahistory
+		JOIN ahistory.semester semester
+		WHERE semester.department = :department
+		AND semester.semesterStartDate < :today
+
+		")
+			->setParameter('department', $department)
+			->setParameter('today', $today)
+			->getResult();
+		return $assistantHistories;
+	}
 	
 }
