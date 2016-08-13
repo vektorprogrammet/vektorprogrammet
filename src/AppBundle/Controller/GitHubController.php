089d7cfa3d641fc33f666bd454579985e59c690c
Kristoffer Ravik Andresen <kra1992@hotmail.com>
2016-08-13 23:50:02 +0200

    Set environment to production for composer (#137)
    * Set environment to production for composer
    
    * Check if GitHub webhook secret exists

--- src/AppBundle/Controller/GitHubController.php
+++ src/AppBundle/Controller/GitHubController.php
@@ -11,6 +11,11 @@ class GitHubController extends Controller
     public function deployAction(Request $request)
     {
         $payload = json_decode($request->getContent(), true);
+        if (!array_key_exists('hook', $payload) || !array_key_exists('config', $payload['hook']) ||
+            !array_key_exists('secret', ['hook']['config'])
+        ) {
+            return new JsonResponse(['status' => 'failed']);
+        }
         $secret = $payload['hook']['config']['secret'];
         $isMaster = array_key_exists('ref', $payload) && $payload['ref'] === 'refs/heads/master';
         $isCorrectSecret = $secret === $this->getParameter('github_webook_secret');
