48aa562aefd0e743ede64fae3a8cc60e5299cb04
Kristoffer Ravik Andresen <kra1992@hotmail.com>
2016-08-14 23:08:35 +0200

    Add composer.phar (no more global composer) (#144)

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ cd $(dirname "$0")
 echo "Starting deploy" >>$log
 date >>$log
 git pull origin master >>$log 2>&1
-SYMFONY_ENV=prod composer install --no-dev --optimize-autoloader >>$log 2>&1
+SYMFONY_ENV=prod php ./composer.phar install --no-dev --optimize-autoloader >>$log 2>&1
 npm install >>$log 2>&1
 ./node_modules/.bin/gulp build:prod >>$log 2>&1
 php app/console cache:clear --env=prod >>$log 2>&1
