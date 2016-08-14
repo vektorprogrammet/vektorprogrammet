da7b256217bab43ffda0413203e3774549540a8a
Kristoffer Ravik Andresen <kra1992@hotmail.com>
2016-08-14 03:42:28 +0200

    Fix deploy logging for stderr output (#141)

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 log=app/logs/deploy.log
 cd $(dirname "$0")
-echo "Starting deploy" >> $log
-date >> $log
-git pull origin master >> $log
-SYMFONY_ENV=prod composer install --no-dev --optimize-autoloader >> $log
-npm install >> $log
-./node_modules/.bin/gulp build:prod >> $log
-php app/console cache:clear --env=prod >> $log
-echo "---------------------------------------------------" >> $log
+echo "Starting deploy" >>$log
+date >>$log
+git pull origin master >>$log 2>&1
+SYMFONY_ENV=prod composer install --no-dev --optimize-autoloader >>$log 2>&1
+npm install >>$log 2>&1
+./node_modules/.bin/gulp build:prod >>$log 2>&1
+php app/console cache:clear --env=prod >>$log 2>&1
+echo "---------------------------------------------------" >>$log
 
 exit 0
