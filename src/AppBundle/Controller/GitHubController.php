03e28b0aa60d8ec49180f285003dd98b451bd61a
kristandre <kra1992@hotmail.com>
2016-08-14 02:02:22 +0200

    Use IP to check if deploy request is from GitHub

--- src/AppBundle/Controller/GitHubController.php
+++ src/AppBundle/Controller/GitHubController.php
@@ -10,22 +10,62 @@ class GitHubController extends Controller
 {
     public function deployAction(Request $request)
     {
-        $payload = json_decode($request->getContent(), true);
-        if (!array_key_exists('hook', $payload) || !array_key_exists('config', $payload['hook']) ||
-            !array_key_exists('secret', ['hook']['config'])
-        ) {
-            return new JsonResponse(['status' => 'failed']);
+        // Check if request is from GitHub
+        $ipIsFromGitHub = $this->ipIsFromGitHub($request->getClientIp());
+        if (!$ipIsFromGitHub) {
+            die('Ip is not from GitHub');
         }
-        $secret = $payload['hook']['config']['secret'];
-        $isMaster = array_key_exists('ref', $payload) && $payload['ref'] === 'refs/heads/master';
-        $isCorrectSecret = $secret === $this->getParameter('github_webook_secret');
 
-        if ($isMaster && $isCorrectSecret) {
+        // Get data
+        $payload = json_decode($request->getContent(), true);
+        $isMaster = array_key_exists('ref', $payload) && $payload['ref'] === 'refs/heads/master';
+
+        // Execute deploy script if there is a push to master
+        if ($isMaster) {
             shell_exec($this->getParameter('kernel.root_dir').'/../deploy.sh');
 
-            return new JsonResponse(['status' => 'success']);
+            return new JsonResponse(['status' => 'Deployed']);
         } else {
-            return new JsonResponse(['status' => 'failed']);
+            return new JsonResponse(['status' => 'Not a master push']);
         }
+    }
+
+    public function ipIsFromGitHub($ip)
+    {
+        // Use curl to create a GET request to the GitHub api
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, 'https://api.github.com/meta');
+        curl_setopt($ch, CURLOPT_HTTPHEADER,
+            array(
+                'User-Agent: NoBrowser v0.1 beta',
+            )
+        );
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $data = curl_exec($ch);
+
+        // Get ip ranges that GitHub uses for their webhooks
+        $ipRanges = json_decode($data, true)['hooks'];
+
+        // Check if ip from request is from GitHub
+        foreach ($ipRanges as $range) {
+            if ($this->cidr_match($ip, $range)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function cidr_match($ip, $range)
+    {
+        list($subnet, $bits) = explode('/', $range);
+        $ip = ip2long($ip);
+        $subnet = ip2long($subnet);
+        $mask = -1 << (32 - $bits);
+        $subnet &= $mask;
+
+        return ($ip & $mask) == $subnet;
     }
 }
