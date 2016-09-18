61aeaf2efa4d14787c4a77c1f14f5240212e8f6a
aestene <arnterik1994@hotmail.com>
2016-09-18 16:56:12 +0200

    Work in progress: New certificate page Added signature_picture_path Button on certificate indexpage

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -65,6 +65,11 @@ class User implements AdvancedUserInterface, \Serializable
     private $picture_path;
 
     /**
+     * @ORM\Column(type="string", length=45, nullable=true)
+     */
+    private $signature_picture_path;
+
+    /**
      * @ORM\Column(type="string", length=45)
      * @Assert\NotBlank(groups={"admission", "create_user"}, message="Dette feltet kan ikke være tomt.")
      */
