a47a8c1b0a2b71c2262f9102e87fd345cb8c32ec
Kristoffer <kra1992@hotmail.com>
2016-09-22 23:13:35 +0200

    Add Doctrine Migrations Bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -25,6 +25,7 @@ class AppKernel extends Kernel
             new SimpleThings\FormExtraBundle\SimpleThingsFormExtraBundle(),
             new Liip\ImagineBundle\LiipImagineBundle(),
             new TFox\MpdfPortBundle\TFoxMpdfPortBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
