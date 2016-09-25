8e557824ff79eaac2d3d4b05455725c9a580dc56
jvals <jvals@users.noreply.github.com>
2016-09-25 18:09:55 +0200

    Get applications in the controller for the school allocation page.

--- src/AppBundle/Controller/SchoolAllocationMainController.php
+++ src/AppBundle/Controller/SchoolAllocationMainController.php
@@ -3,11 +3,23 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Entity\Application;
+use AppBundle\Entity\Semester;
+use Symfony\Component\HttpFoundation\Request;
 
 class SchoolAllocationMainController extends Controller
 {
-    public function indexAction()
+    public function showAction(Request $request, $departmentId = null)
     {
-        return $this->render('school_admin/school_allocate_main.html.twig');
+        $user = $this->get('security.token_storage')->getToken()->getUser();
+        if (null === $departmentId) {
+            $departmentId = $user->getFieldOfStudy()->getDepartment()->getId();
+        }
+        $currentSemester = $this->getDoctrine()->getRepository('AppBundle:Semester')->findCurrentSemesterByDepartment($departmentId);
+        $applications = $this->getDoctrine()->getRepository('AppBundle:Application')->findAllAllocatableApplicationsBySemester($currentSemester);
+
+        return $this->render('school_admin/school_allocate_main.html.twig', array(
+            'applications' => $applications,
+        ));
     }
 }
