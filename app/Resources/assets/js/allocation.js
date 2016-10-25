7abacb4cfd4e75c5bd6d0a0ac7529eee555c3b2e
jvals <jvals@users.noreply.github.com>
2016-10-25 22:57:54 +0200

    Add school column which is updated based on selected day

--- app/Resources/assets/js/allocation.js
+++ app/Resources/assets/js/allocation.js
@@ -1,13 +1,17 @@
 /**
  * Created by jorgenvalstad on 03.10.2016.
  */
+
+var school_availability;
+
 $.get("/kontrollpanel/api/assistants", function (data) {
 
 })
 
 function getAvailableDays(assistant) {
     // NOTE: The select id is not unique if two people have the same name.
-    var spaceless_name = assistant['name'].split(' ').join('');
+    var name = assistant['name'];
+    var spaceless_name = string_remove_space(name);
     var select = $('<select>').attr('id', spaceless_name);
     var availability = assistant['availability'];
     for (var i in availability) {
@@ -19,10 +23,23 @@ function getAvailableDays(assistant) {
         }
     }
     select.change(function () {
-        updateAvailableSchools(select.val())
+        updateAvailableSchools(select.val(), name);
     })
-    return select
+    select.addClass("allocation_select");
+    return select;
 }
+
+
+function generateSchoolSelect(assistant) {
+    var name = assistant['name'];
+    var spaceless_name = string_remove_space(name);
+    var school_select_id = spaceless_name.concat("SchoolSelect");
+    var select = $('<select>').attr('id', school_select_id);
+    select.addClass("allocation_select");
+    return select;
+}
+
+
 $.get("/kontrollpanel/api/allocated_assistants", function (data) {
     //Adding a row in allocation_table for each assistant
     var assistants = JSON.parse(data);
@@ -39,17 +56,35 @@ $.get("/kontrollpanel/api/allocated_assistants", function (data) {
                 .append($('<td>')
                     .append(getAvailableDays(assistant))
                 )
+                // One column for school selection
+                .append($('<td>')
+                    .append(generateSchoolSelect(assistant))
+                )
             );
     }
 });
 
 $.get("/kontrollpanel/api/schools_and_days", function (data) {
-    var schools = JSON.parse(data);
-    console.log(schools);
+    school_availability = JSON.parse(data);
 });
 /*
 * Update the select options based on the chosen day
 * */
-function updateAvailableSchools(day) {
+function updateAvailableSchools(day, assistant_name) {
+    var school_select_id = string_remove_space(assistant_name).concat("SchoolSelect");
+    var school_select = $("#".concat(school_select_id));
+    school_select.find('option').remove();
+    for(var i in school_availability[day]) {
+        school_select.append($('<option>', {
+            value: school_availability[day][i],
+            text: school_availability[day][i]
+        }));
+    }
 
+}
+/*
+* Remove space from string
+* */
+function string_remove_space(str) {
+    return str.split(' ').join('');
 }
