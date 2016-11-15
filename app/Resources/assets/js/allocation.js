ba72c101f0a54307cb4b7885b6d15ab83c9b7ace
Kristoffer <kra1992@hotmail.com>
2016-11-15 01:32:48 +0100

    Synchronize available days, groups and schools

--- app/Resources/assets/js/allocation.js
+++ app/Resources/assets/js/allocation.js
@@ -2,153 +2,265 @@
  * Created by jorgenvalstad on 03.10.2016.
  */
 
-var school_availability;
-
-$.get("/kontrollpanel/api/assistants", function (data) {
-
-})
-
-function getAvailableDays(assistant) {
-    // NOTE: The select id is not unique if two people have the same name.
-    var name = assistant['name'];
-    var spaceless_name = string_remove_space(name);
-    var select = $('<select>').attr('id', spaceless_name);
-    var availability = assistant['availability'];
-    for (var i in availability) {
-        if (availability[i]) {
-            select.append($('<option>', {
-                value: i,
-                text: i
-            }));
-        }
-    }
-    select.change(function () {
-        updateAvailableSchools(select.val(), name);
-    })
-    select.addClass("allocation_select");
-    return select;
-}
-
-
-function generateSchoolSelect(assistant) {
-    var name = assistant['name'];
-    var spaceless_name = string_remove_space(name);
-    var school_select_id = spaceless_name.concat("SchoolSelect");
-    var select = $('<select>').attr('id', school_select_id);
-    select.addClass("allocation_select");
-    return select;
-}
+var schools = [];
+var assistants = [];
+getSchools();
 
 /*
-Generate group selector based on the assistant's preference
- */
-function generateGroupSelect(assistant) {
-    var name = assistant['name'];
-    var spaceless_name = string_remove_space(name);
-    var select = $('<select>');
-    select.addClass("allocation_select");
-    var preferredGroup = assistant['preferredGroup'];
-    if (!assistant['doublePosition']) {
-        switch (preferredGroup) {
-            case null:
-                select.append($('<option>', {value: "Bolk 1", text: "Bolk 1"}));
-                select.append($('<option>', {value: "Bolk 2", text: "Bolk 2"}));
-                break;
-            case 1:
-                select.append($('<option>', {value: "Bolk 1", text: "Bolk 1"}));
-                break;
-            case 2:
-                select.append($('<option>', {value: "Bolk 2", text: "Bolk 2"}));
-                break;
-        }
-    } else {
-        select.append($('<option>', {value: "Dobbel", text: "Dobbel"}));
-    }
-    return select;
-}
-
-$.get("/kontrollpanel/api/schools_and_days", function (data) {
-    school_availability = JSON.parse(data);
-    generateAllocationTable();
-});
-
-/*
-Generate the allocation table
- */
-function generateAllocationTable() {
-    $.get("/kontrollpanel/api/allocated_assistants", function (data) {
-        //Adding a row in allocation_table for each assistant
-        var assistants = JSON.parse(data);
-        for (var i in assistants) {
-            var assistant = assistants[i];
-            var name = assistant.name;
-            $('.allocation_table')
-                .append($('<tr>')
-                    // One column for name
-                        .append($('<td>')
-                            .text(name)
-                        )
-                        // One column for day selection
-                        .append($('<td>')
-                            .append(getAvailableDays(assistant))
-                        )
-                        // One column for school selection
-                        .append($('<td>')
-                            .append(generateSchoolSelect(assistant))
-                        )
-                        // One column for group selection
-                        .append($('<td>')
-                            .append(generateGroupSelect(assistant))
-                        )
-                );
-
-            // Initialize the school selector
-            var day_select = $("#".concat(string_remove_space(name)));
-            updateAvailableSchools(day_select.val(), name);
-            var school_select = $("#".concat(string_remove_space(name).concat("SchoolSelect")));
-            // Set the school and day that the algorithm chose
-            if(assistant.assignedDay != null) {
-                day_select.val(norwegian_day(assistant.assignedDay));
-            }
-            if(assistant.assignedSchool != null) {
-                school_select.val(assistant.assignedSchool);
-            }
-        }
-    });
-}
-
-/*
-* Update the select options based on the chosen day
-* */
-function updateAvailableSchools(day, assistant_name) {
-    var school_select_id = string_remove_space(assistant_name).concat("SchoolSelect");
-    var school_select = $("#".concat(school_select_id));
-    school_select.find('option').remove();
-    for(var i in school_availability[day]) {
-        school_select.append($('<option>', {
-            value: school_availability[day][i],
-            text: school_availability[day][i]
-        }));
-    }
-}
-
-/*
-* Remove space from string
-* */
+ * Remove space from string
+ * */
 function string_remove_space(str) {
     return str.split(' ').join('');
 }
 
-
 /*
-Convert english day to norwegian day
+ Convert english day to norwegian day
  */
 function norwegian_day(english_day) {
     switch (english_day) {
-        case "Monday":    return "Mandag";
-        case "Tuesday":   return "Tirsdag";
-        case "Wednesday": return "Onsdag";
-        case "Thursday":  return "Torsdag";
-        case "Friday":    return "Fredag";
+        case "Monday":
+            return "Mandag";
+        case "Tuesday":
+            return "Tirsdag";
+        case "Wednesday":
+            return "Onsdag";
+        case "Thursday":
+            return "Torsdag";
+        case "Friday":
+            return "Fredag";
     }
+}
+
+function generateAllocationTable() {
+    //Adding a row in allocation_table for each assistant
+    assistants.forEach(function (assistant) {
+        $('.allocation_table').append(assistant.getTableRow());
+
+        // Initialize the school selector
+        var day_select = $("#".concat(string_remove_space(name)));
+        // updateAvailableSchools(day_select.val(), name);
+        assistant.updateAvailableSchools();
+        var school_select = $("#".concat(string_remove_space(name).concat("SchoolSelect")));
+        // Set the school and day that the algorithm chose
+        if (assistant.assignedDay != null) {
+            day_select.val(norwegian_day(assistant.assignedDay));
+        }
+        if (assistant.assignedSchool != null) {
+            school_select.val(assistant.assignedSchool);
+        }
+    });
+}
+
+function getSchools() {
+    $.get("/kontrollpanel/api/schools", function (data) {
+        JSON.parse(data).forEach(function (s) {
+            schools.push(new School(s));
+        });
+        getAssistants();
+    });
+}
+
+function getAssistants() {
+    $.get("/kontrollpanel/api/assistants", function (data) {
+        JSON.parse(data).forEach(function (a) {
+            assistants.push(new Assistant(a));
+        });
+        generateAllocationTable();
+    });
+}
+
+function Assistant(assistantData) {
+    this.name = assistantData.name;
+    this.group = assistantData.group;
+    this.doublePosition = assistantData.doublePosition;
+    this.preferredGroup = assistantData.preferredGroup;
+    this.assignedSchool = null;
+    this.assignedDay = assistantData.assignedDay;
+    this.availability = assistantData.availability;
+    this.selectedDay = '';
+
+    this.getTableRow = function () {
+        return $('<tr>')
+        // One column for name
+            .append($('<td>')
+                .text(this.name)
+            )
+            // One column for day selection
+            .append($('<td>')
+                .append(this.getAvailableDaysSelect())
+            )
+            // One column for group selection
+            .append($('<td>')
+                .append(this.getGroupSelect())
+            )
+            // One column for school selection
+            .append($('<td>')
+                .append(this.getSchoolsSelect())
+            );
+    };
+
+    this.getAvailableDaysSelect = function () {
+        var name = this.name;
+        var spaceless_name = string_remove_space(name);
+        var select = $('<select>').attr('id', spaceless_name);
+        for (var day in this.availability) {
+            if (this.availability.hasOwnProperty(day) && this.availability[day]) {
+                select.append($('<option>', {
+                    value: day,
+                    text: norwegian_day(day)
+                }));
+            }
+        }
+        select.change($.proxy(function () {
+            var prevSchool = this.assignedSchool;
+            this.assignedSchool = null;
+            if(prevSchool !== null) {
+                prevSchool.updateCapacityLeft();
+            }
+            this.selectedDay = select.val();
+            updateAllAvailableSchools();
+        }, this));
+        select.addClass("allocation_select");
+        return select;
+    };
+
+    this.getSchoolsSelect = function () {
+        var spaceless_name = string_remove_space(this.name);
+        var school_select_id = spaceless_name.concat("SchoolSelect");
+        var select = $('<select>').attr('id', school_select_id);
+        select.addClass("allocation_select");
+        return select;
+    };
+
+    this.getGroupSelect = function () {
+        var select = $('<select>').attr('id', string_remove_space(this.name).concat("GroupSelect"));
+        select.addClass("allocation_select");
+        var preferredGroup = this.preferredGroup;
+        if (!this.doublePosition) {
+            switch (preferredGroup) {
+                case null:
+                    select.append($('<option>', {value: "Bolk 1", text: "Bolk 1"}));
+                    select.append($('<option>', {value: "Bolk 2", text: "Bolk 2"}));
+                    break;
+                case 1:
+                    select.append($('<option>', {value: "Bolk 1", text: "Bolk 1"}));
+                    break;
+                case 2:
+                    select.append($('<option>', {value: "Bolk 2", text: "Bolk 2"}));
+                    break;
+            }
+        } else {
+            select.append($('<option>', {value: "Dobbel", text: "Dobbel"}));
+            select.append($('<option>', {value: "Bolk 1", text: "Bolk 1"}));
+            select.append($('<option>', {value: "Bolk 2", text: "Bolk 2"}));
+        }
+        select.change($.proxy(function () {
+            this.updateAvailableSchools();
+        }, this));
+        return select;
+    };
+
+    this.getSelectedDay = function () {
+        return $('#' + string_remove_space(this.name)).val();
+    };
+
+    this.getSelectedGroup = function () {
+        return $('#' + string_remove_space(this.name).concat("GroupSelect")).val();
+    };
+
+    this.updateAvailableSchools = function () {
+        var availableSchools = getAvailableSchoolsByDay(this.getSelectedGroup(), this.getSelectedDay());
+        var spaceless_name = string_remove_space(this.name);
+        var school_select_id = spaceless_name.concat("SchoolSelect");
+        var select = $('#' + school_select_id);
+        select.empty();
+        var option = $('<option>', {value: '', text: 'Velg skole'});
+        select.append(option);
+        availableSchools.forEach(function (school) {
+            var option = $('<option>', {value: school.name, text: school.name});
+            if (this.assignedSchool !== null && this.assignedSchool.name === school.name) {
+                option.attr('selected', true)
+            }
+            select.append(option);
+        }, this);
+        select.change($.proxy(function () {
+            var school = getSchoolByName(select.val());
+            var prevSchool = this.assignedSchool;
+            this.assignedSchool = school;
+            if (prevSchool !== null) {
+                prevSchool.updateCapacityLeft();
+            }
+            if (school !== null) {
+                school.updateCapacityLeft();
+            }
+            updateAllAvailableSchools();
+        }, this))
+    };
+}
+
+function School(schoolData) {
+    this.name = schoolData.name;
+    this.capacity = schoolData.capacity;
+    this.capacityLeft = JSON.parse(JSON.stringify(schoolData.capacity));
+
+    this.hasCapacityLeftOnDay = function (group, day) {
+        switch (group) {
+            case 'Bolk 1':
+                return this.capacityLeft['1'][day] > 0;
+            case 'Bolk 2':
+                return this.capacityLeft['2'][day] > 0;
+            case 'Dobbel':
+                return this.capacityLeft['1'][day] > 0 && this.capacityLeft['2'][day] > 0;
+        }
+        return false;
+    };
+
+    this.updateCapacityLeft = function () {
+        var cl = JSON.parse(JSON.stringify(this.capacity));
+        assistants.forEach(function (assistant) {
+            if (assistant.assignedSchool !== null && assistant.assignedSchool.name === this.name) {
+                switch (assistant.getSelectedGroup()) {
+                    case 'Bolk 1':
+                        cl['1'][assistant.getSelectedDay()] -= 1;
+                        break;
+                    case 'Bolk 2':
+                        cl['2'][assistant.getSelectedDay()] -= 1;
+                        break;
+                    case 'Dobbel':
+                        cl['1'][assistant.getSelectedDay()] -= 1;
+                        cl['2'][assistant.getSelectedDay()] -= 1;
+                        break;
+                }
+            }
+        }, this);
+        this.capacityLeft = cl;
+    }
+}
+
+function getAvailableSchoolsByDay(group, day) {
+    var availableSchools = [];
+    schools.forEach(function (school) {
+        if (school.hasCapacityLeftOnDay(group, day)) {
+            availableSchools.push(school);
+        }
+    });
+    return availableSchools;
+}
+
+function getSchoolByName(name) {
+    for (var i = 0; i < schools.length; i++) {
+        var school = schools[i];
+        if (school.name === name) {
+            return school;
+        }
+    }
+    return null;
+}
+
+function updateAllAvailableSchools() {
+    assistants.forEach(function (assistant) {
+        if (assistant.assignedSchool === null) {
+            assistant.updateAvailableSchools();
+        }
+    })
 }
