71768a81c2f6870428f8f921373c37983f1c5cd8
kristandre <kra1992@hotmail.com>
2017-01-03 14:56:04 +0100

    Move subscriber actions to SubscriberController

--- src/AppBundle/Controller/NewsletterController.php
+++ src/AppBundle/Controller/NewsletterController.php
@@ -2,16 +2,12 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Entity\Department;
 use AppBundle\Entity\Letter;
 use AppBundle\Entity\Newsletter;
-use AppBundle\Entity\Subscriber;
 use AppBundle\Form\Type\CreateLetterType;
-use AppBundle\Form\Type\SubscribeToNewsletterType;
 use AppBundle\Form\Type\CreateNewsletterType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class NewsletterController extends Controller
 {
@@ -54,53 +50,6 @@ class NewsletterController extends Controller
         ));
     }
 
-    public function showSubscribePageAction(Request $request, Newsletter $newsletter)
-    {
-        $subscriber = new Subscriber();
-
-        $form = $this->createForm(new SubscribeToNewsletterType(), $subscriber);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $alreadySubscribed = count($this->getDoctrine()->getRepository('AppBundle:Subscriber')->
-                findByEmailAndNewsletter($subscriber->getEmail(), $newsletter)) > 0;
-
-            if (!$alreadySubscribed) {
-                $subscriber->setNewsletter($newsletter);
-
-                $manager = $this->getDoctrine()->getManager();
-                $manager->persist($subscriber);
-                $manager->flush();
-            }
-
-            $this->addFlash('success', $subscriber->getEmail().' ble registrert');
-
-            return $this->redirectToRoute('newsletter_show_subscribe', array('id' => $newsletter->getId()));
-        }
-
-        return $this->render('newsletter/subscribe.html.twig', array(
-            'newsletter' => $newsletter,
-            'form' => $form->createView(),
-        ));
-    }
-
-    public function showSubscribeAction(Department $department)
-    {
-        $newsletter = $this->getDoctrine()->getRepository('AppBundle:Newsletter')->findCheckedByDepartment($department);
-
-        return $this->render('', array(
-            'newsletters' => $newsletter,
-        ));
-    }
-
-    public function showSubscribersAction(Newsletter $newsletter)
-    {
-        return $this->render('newsletter/show_subscribers.html.twig', array(
-            'newsletter' => $newsletter,
-        ));
-    }
-
     public function deleteAction(Newsletter $newsletter)
     {
         $manager = $this->getDoctrine()->getManager();
@@ -108,17 +57,6 @@ class NewsletterController extends Controller
         $manager->flush();
 
         return $this->redirectToRoute('newsletter_show_all');
-    }
-
-    public function adminUnsubscribeAction(Subscriber $subscriber)
-    {
-        $newsletterId = $subscriber->getNewsletter()->getId();
-
-        $manager = $this->getDoctrine()->getManager();
-        $manager->remove($subscriber);
-        $manager->flush();
-
-        return $this->redirectToRoute('newsletter_show_subscribers', array('id' => $newsletterId));
     }
 
     public function showOnAdmissionPageAction(Newsletter $newsletter)
@@ -139,45 +77,6 @@ class NewsletterController extends Controller
         $manager->flush();
 
         return $this->redirectToRoute('newsletter_show_all');
-    }
-
-    public function subscribeFormAction(Request $request, Department $department)
-    {
-        $manager = $this->getDoctrine()->getManager();
-        $newsletter = $manager->getRepository('AppBundle:Newsletter')->findCheckedByDepartment($department);
-
-        $subscriber = new Subscriber();
-
-        if ($newsletter !== null) {
-            $form = $this->createForm(new SubscribeToNewsletterType(), $subscriber, array(
-                'action' => $this->generateUrl('newsletter_subscribe', array(
-                    'id' => $newsletter->getId(),
-                )),
-            ));
-        } else {
-            $form = $this->createForm(new SubscribeToNewsletterType(), $subscriber);
-        }
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $alreadySubscribed = count($manager->getRepository('AppBundle:Subscriber')->findByEmailAndNewsletter($subscriber->getEmail(), $newsletter)) > 0;
-            if (!$alreadySubscribed) {
-                $subscriber->setNewsletter($newsletter);
-                $manager = $this->getDoctrine()->getManager();
-                $manager->persist($subscriber);
-                $manager->flush();
-            }
-
-            $this->addFlash('admission-notice', 'Takk for at du meldte deg på '.$newsletter->getName().'! '.$subscriber->getEmail().' ble registrert');
-
-            return $this->redirectToRoute('admission_show_specific_department', array('id' => $department->getId()));
-        }
-
-        return $this->render('newsletter/subscribe_form.html.twig', array(
-            'newsletter' => $newsletter,
-            'form' => $form->createView(),
-        ));
     }
 
     public function createLetterAction(Newsletter $newsletter, Request $request)
@@ -250,42 +149,6 @@ class NewsletterController extends Controller
     {
         return $this->render('newsletter/show_letter_content.html.twig', array(
             'letter' => $letter,
-        ));
-    }
-
-    public function unsubscribeNewsletterAction($unsubscribeCode)
-    {
-        $subscriber = $this->getDoctrine()->getRepository('AppBundle:Subscriber')->findOneBy(array(
-            'unsubscribeCode' => $unsubscribeCode,
-        ));
-        if ($subscriber === null) {
-            throw new NotFoundHttpException();
-        }
-        $manager = $this->getDoctrine()->getManager();
-        $manager->remove($subscriber);
-        $manager->flush();
-
-        return $this->redirectToRoute('unsubscribe_newsletter_confirmation', array(
-            'email' => $subscriber->getEmail(),
-            'newsletter' => $subscriber->getNewsletter()->getId(),
-        ));
-    }
-
-    public function unsubscribeNewsletterConfirmationAction(Request $request)
-    {
-        $email = $request->query->get('email');
-        $newsletterId = $request->query->get('newsletter');
-        if ($email === null || $newsletterId === null) {
-            throw new NotFoundHttpException();
-        }
-        $newsletter = $this->getDoctrine()->getRepository('AppBundle:Newsletter')->find($newsletterId);
-        if ($newsletter === null) {
-            throw new NotFoundHttpException();
-        }
-
-        return $this->render('newsletter/unsubscribe.html.twig', array(
-            'email' => $email,
-            'newsletter' => $newsletter,
         ));
     }
 }
