f9dda6dcb056cdbda7b3d766d3dd2fd3633937f2
jvals <jvals@users.noreply.github.com>
2017-02-10 19:58:38 +0100

    Revert unrelated files back to original state

--- src/AppBundle/Form/Type/CreateUserOnApplicationType.php
+++ src/AppBundle/Form/Type/CreateUserOnApplicationType.php
@@ -3,7 +3,6 @@
 namespace AppBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
