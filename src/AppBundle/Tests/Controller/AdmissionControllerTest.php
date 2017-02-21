7e404701da7b39777387e1ae9f4bc42a9e370646
jvals <jvals@users.noreply.github.com>
2017-02-21 17:36:27 +0100

    Add tests for admission page with focus on newsletters

---
+++ src/AppBundle/Tests/Controller/AdmissionControllerTest.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace AppBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class AdmissionControllerTest extends WebTestCase
+{
+    public function testShow()
+    {
+    }
+
+    public function testCreateWantNewsletterApplication()
+    {
+
+        // Admin
+        $clientAdmin = static::createClient(array(), array(
+            'PHP_AUTH_USER' => 'admin',
+            'PHP_AUTH_PW' => '1234',
+        ));
+
+        // Go to AdmissionTestList
+        $crawler = $clientAdmin->request('GET', '/kontrollpanel/nyhetsbrev/4');
+        $this->assertTrue($clientAdmin->getResponse()->isSuccessful());
+
+        $applicationsBefore = $crawler->filter('tr')->count();
+
+        // Submit an application
+        // User
+        $clientAnonymous = static::createClient();
+
+        $crawler = $clientAnonymous->request('GET', '/opptak/avdeling/1');
+        $this->assertTrue($clientAnonymous->getResponse()->isSuccessful());
+
+        $submitButton = $crawler->selectButton('Søk');
+        $form = $submitButton->form();
+
+        $form['application[user][firstName]'] = 'Newsletter Please';
+        $form['application[user][lastName]'] = 'Newsletter Please';
+        $form['application[user][email]'] = 'j@vektorprogrammet.no';
+        $form['application[user][phone]'] = '99887766';
+        $form['application[user][fieldOfStudy]'] = 2;
+        $form['application[user][gender]'] = 0;
+        $form['application[yearOfStudy]'] = 4;
+        $form['application[wantsNewsletter]'] = true;
+
+        $clientAnonymous->submit($form);
+
+        $crawler = $clientAdmin->request('GET', '/kontrollpanel/nyhetsbrev/4');
+        $this->assertTrue($clientAdmin->getResponse()->isSuccessful());
+
+        $applicationsAfter = $crawler->filter('tr')->count();
+
+        $this->assertEquals($applicationsBefore + 1, $applicationsAfter);
+        \TestDataManager::restoreDatabase();
+    }
+
+    public function testCreateNotWantNewsletterApplication()
+    {
+
+        // Admin
+        $clientAdmin = static::createClient(array(), array(
+            'PHP_AUTH_USER' => 'admin',
+            'PHP_AUTH_PW' => '1234',
+        ));
+
+        // Go to AdmissionTestList
+        $crawler = $clientAdmin->request('GET', '/kontrollpanel/nyhetsbrev/4');
+        $this->assertTrue($clientAdmin->getResponse()->isSuccessful());
+
+        $applicationsBefore = $crawler->filter('tr')->count();
+
+        // Submit an application
+        // User
+        $clientAnonymous = static::createClient();
+
+        $crawler = $clientAnonymous->request('GET', '/opptak/avdeling/1');
+        $this->assertTrue($clientAnonymous->getResponse()->isSuccessful());
+
+        $submitButton = $crawler->selectButton('Søk');
+        $form = $submitButton->form();
+
+        $form['application[user][firstName]'] = 'No newsletters please';
+        $form['application[user][lastName]'] = 'No newsletters please';
+        $form['application[user][email]'] = 'j@vektorprogrammet.no';
+        $form['application[user][phone]'] = '99887766';
+        $form['application[user][fieldOfStudy]'] = 2;
+        $form['application[user][gender]'] = 0;
+        $form['application[yearOfStudy]'] = 4;
+        $form['application[wantsNewsletter]'] = false;
+
+        $clientAnonymous->submit($form);
+
+        $crawler = $clientAdmin->request('GET', '/kontrollpanel/nyhetsbrev/4');
+        $this->assertTrue($clientAdmin->getResponse()->isSuccessful());
+
+        $applicationsAfter = $crawler->filter('tr')->count();
+
+        $this->assertEquals($applicationsBefore, $applicationsAfter);
+        \TestDataManager::restoreDatabase();
+    }
+}
