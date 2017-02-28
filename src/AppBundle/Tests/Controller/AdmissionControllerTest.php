d4bf094c99e76cce3e868a7341b857c38e6ac10e
MaynardTheMenace <thusansjef@gmail.com>
2017-02-28 17:37:28 +0100

    Laget tester for å sjekke de nye funksjonene

--- src/AppBundle/Tests/Controller/AdmissionControllerTest.php
+++ src/AppBundle/Tests/Controller/AdmissionControllerTest.php
@@ -12,11 +12,13 @@ class AdmissionControllerTest extends WebTestCase
 
     public function testCreateWantNewsletterApplication()
     {
-        $applicationsBefore = $this->countRows();
+        $path = '/kontrollpanel/nyhetsbrev/4';
+
+        $applicationsBefore = $this->countRows($path);
 
         $this->createAndSubmitForm('Love newsletters Peter', true);
 
-        $applicationsAfter = $this->countRows();
+        $applicationsAfter = $this->countRows($path);
 
         $this->assertEquals($applicationsBefore + 1, $applicationsAfter);
         \TestDataManager::restoreDatabase();
@@ -24,20 +26,52 @@ class AdmissionControllerTest extends WebTestCase
 
     public function testCreateNotWantNewsletterApplication()
     {
-        $applicationsBefore = $this->countRows();
+        $path = '/kontrollpanel/nyhetsbrev/4';
+
+        $applicationsBefore = $this->countRows($path);
 
         $this->createAndSubmitForm('No newsletter Johnson', false);
 
-        $applicationsAfter = $this->countRows();
+        $applicationsAfter = $this->countRows($path);
+
+        $this->assertEquals($applicationsBefore, $applicationsAfter);
+        \TestDataManager::restoreDatabase();
+    }
+
+    public function testCreateWantTeamInterest()
+    {
+        $path = '/kontrollpanel/opptakadmin/teaminteresse/2';
+
+        $applicationsBefore = $this->countRows($path);
+
+        $this->createAndSubmitForm_teamInterest(true);
+
+        $applicationsAfter = $this->countRows($path);
+
+        $this->assertEquals($applicationsBefore + 1, $applicationsAfter);
+        \TestDataManager::restoreDatabase();
+    }
+
+    public function testCreateNotWantTeamInterest()
+    {
+        $path = '/kontrollpanel/opptakadmin/teaminteresse/2';
+
+        $applicationsBefore = $this->countRows($path);
+
+        $this->createAndSubmitForm_teamInterest(false);
+
+        $applicationsAfter = $this->countRows($path);
 
         $this->assertEquals($applicationsBefore, $applicationsAfter);
         \TestDataManager::restoreDatabase();
     }
 
     /**
+     * @param string $path
+     *
      * @return int
      */
-    private function countRows():int
+    public function countRows(string $path):int
     {
         // Admin
         $clientAdmin = static::createClient(array(), array(
@@ -46,7 +80,7 @@ class AdmissionControllerTest extends WebTestCase
         ));
 
         // Go to AdmissionTestList
-        $crawler = $clientAdmin->request('GET', '/kontrollpanel/nyhetsbrev/4');
+        $crawler = $clientAdmin->request('GET', $path);
         $this->assertTrue($clientAdmin->getResponse()->isSuccessful());
 
         return $crawler->filter('tr')->count();
@@ -74,6 +108,36 @@ class AdmissionControllerTest extends WebTestCase
         $form['application[user][gender]'] = 0;
         $form['application[yearOfStudy]'] = 4;
         $form['application[wantsNewsletter]'] = $wantsNewsletter;
+
+        $clientAnonymous->submit($form);
+    }
+
+    /**
+     * @param bool $teamInterest
+     */
+    private function createAndSubmitForm_teamInterest(bool $teamInterest)
+    {
+        $clientAnonymous = static::createClient(array(), array(
+            'PHP_AUTH_USER' => 'assistent',
+            'PHP_AUTH_PW' => '1234',
+        ));
+
+        $crawler = $clientAnonymous->request('GET', '/opptak/eksisterende');
+        $this->assertTrue($clientAnonymous->getResponse()->isSuccessful());
+
+        $submitButton = $crawler->selectButton('Søk');
+        $form = $submitButton->form();
+
+        $form['application[yearOfStudy]'] = 3;
+        $form['application[applicationPractical][monday]'] = 'Bra';
+        $form['application[applicationPractical][tuesday]'] = 'Ikke';
+        $form['application[applicationPractical][wednesday]'] = 'Bra';
+        $form['application[applicationPractical][thursday]'] = 'Ikke';
+        $form['application[applicationPractical][friday]'] = 'Bra';
+        $form['application[applicationPractical][doublePosition]'] = 0;
+        $form['application[applicationPractical][preferredGroup]'] = 'Bolk 1';
+        $form['application[applicationPractical][english]'] = 1;
+        $form['application[applicationPractical][teamInterest]'] = $teamInterest;
 
         $clientAnonymous->submit($form);
     }
