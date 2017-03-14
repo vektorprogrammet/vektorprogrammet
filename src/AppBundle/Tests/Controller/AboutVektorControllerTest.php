7ae3669c75797f9bdfa22e5cddaf0bb308911913
aestene <aestene@users.noreply.github.com>
2017-03-14 20:36:22 +0100

    Revert "Use BaseWebTestCase in AboutVektorControllerTest (#500)"
    This reverts commit 9a18fcf4ce29d293ee18105ea3989846b8fcbe7f.

--- src/AppBundle/Tests/Controller/AboutVektorControllerTest.php
+++ src/AppBundle/Tests/Controller/AboutVektorControllerTest.php
@@ -2,14 +2,17 @@
 
 namespace AppBundle\Tests\Controller;
 
-use AppBundle\Tests\BaseWebTestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class AboutVektorControllerTest extends BaseWebTestCase
+class AboutVektorControllerTest extends WebTestCase
 {
     public function testShow()
     {
-        $crawler = $this->anonymousGoTo('/omvektor');
+        $client = static::createClient();
 
+        $crawler = $client->request('GET', '/omvektor');
+
+        // Assert that we have the correct amount of data
         $this->assertEquals(1, $crawler->filter('h1:contains("Om Vektorprogrammet")')->count());
         $this->assertEquals(1, $crawler->filter('h1:contains("Mål og verdier")')->count());
         $this->assertEquals(1, $crawler->filter('h1:contains("Historie")')->count());
@@ -17,12 +20,18 @@ class AboutVektorControllerTest extends BaseWebTestCase
         $this->assertEquals(1, $crawler->filter('strong:contains("Vektorprogrammet arbeider for å øke interessen for matematikk og realfag blant elever i grunnskolen. ")')->count());
         $this->assertEquals(1, $crawler->filter('li:contains("Vektorprogrammet ønsker å øke matematikkforståelsen blant elever i grunnskolen.")')->count());
         $this->assertEquals(1, $crawler->filter('p:contains("Vektorprogrammet er et initiativ som ble startet av linjeforeningen Nabla i september 2010")')->count());
+
+        // Assert a specific 200 status code
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
 
     public function testShowFaq()
     {
-        $crawler = $this->anonymousGoTo('/faq');
+        $client = static::createClient();
 
+        $crawler = $client->request('GET', '/faq');
+
+        // Assert that we have the correct amount of data
         $this->assertEquals(1, $crawler->filter('h1:contains("Ofte stilte spørsmål")')->count());
     }
 }
