a6ef7d831adf5fe4b48679a8daa91dc385b5946c
sigtot <sigurdtotland@gmail.com>
2017-03-17 10:47:27 +0100

    Removed unused use statement in controller

--- src/AppBundle/Controller/SubstituteController.php
+++ src/AppBundle/Controller/SubstituteController.php
@@ -5,26 +5,16 @@ namespace AppBundle\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AppBundle\Entity\Semester;
-use AppBundle\Entity\Department;
 use AppBundle\Entity\Application;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use AppBundle\Form\Type\ModifySubstituteType;
 
 /**
  * SubstituteController is the controller responsible for substitute assistants,
- * such as showing and deleting substitutes.
+ * such as showing, modifying and deleting substitutes.
  */
 class SubstituteController extends Controller
 {
-    public function showAction()
-    {
-        // No department specified, get the user's department and call showBySemester with
-        // either current or latest semester for that department
-        $department = $this->getUser()->getDepartment();
-
-        return $this->showBySemesterAction($department->getCurrentOrLatestSemester());
-    }
-
     public function showBySemesterAction(Semester $semester)
     {
         $substitutes = $this->getDoctrine()->getRepository('AppBundle:Application')->findSubstitutesBySemester($semester);
@@ -33,6 +23,15 @@ class SubstituteController extends Controller
             'substitutes' => $substitutes,
             'semester' => $semester,
         ));
+    }
+
+    public function showAction()
+    {
+        // No department specified, get the user's department and call showBySemester with
+        // either current or latest semester for that department
+        $department = $this->getUser()->getDepartment();
+
+        return $this->showBySemesterAction($department->getCurrentOrLatestSemester());
     }
 
     public function showModifyFormAction(Request $request, Application $application)
