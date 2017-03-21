377779841106c2ea5dc114ffdbf171b74e9775d8
jvals <jvals@users.noreply.github.com>
2017-03-21 16:22:24 +0100

    Implement tests for accept and reschedule

--- src/AppBundle/Tests/Controller/AdmissionAdminControllerTest.php
+++ src/AppBundle/Tests/Controller/AdmissionAdminControllerTest.php
@@ -235,4 +235,65 @@ class AdmissionAdminControllerTest extends BaseWebTestCase
         $this->assertEquals(0, $crawler->filter('td:contains("Ruben")')->count());
         $this->assertEquals(0, $crawler->filter('td:contains("Ravnå")')->count());
     }
+
+    /**
+     * Test the functions on /intervju/code.
+     */
+    public function testResponseInterview()
+    {
+        // Test accept
+        $this->testStatus('Akseptert', 'Godta', 'Intervjuet ble akseptert.');
+
+        // Test reschedule
+        $this->testStatus('Ny tid ønskes', 'Be om ny tid', 'Forespørsel har blitt sendt.');
+
+        // Test cancel
+        // TODO
+    }
+
+    /**
+     * Test the status functionality on /intervju/code.
+     *
+     * Start at kontrollpanel/opptak/fordelt and count occurrences of
+     * $status and "Ingen svar". Proceed to the schedule response page
+     * for our designated fixture and click the button corresponding
+     * to $button_text. Then, verify that we get the correct flash
+     * message after the redirect. Finally, go back to assigned page
+     * and check that the number of elements containing $status has
+     * increased and that the number of elements containing
+     * "Ingen svar" har decreased.
+     *
+     * @param string $status
+     * @param string $button_text
+     * @param string $flash_text
+     */
+    private function testStatus(string $status, string $button_text, string $flash_text)
+    {
+        $crawler = $this->teamMemberGoTo('/kontrollpanel/opptak/fordelt');
+
+        // We store these values, because we expect them to change soon
+        $count_no_answer = $crawler->filter('td:contains("Ingen svar")')->count();
+        $count_status = $crawler->filter('td:contains('.$status.')')->count();
+
+        // "code" is a special response code used in the dev environment
+        $crawler = $this->anonymousGoTo('/intervju/code');
+
+        // Clicking a button on this page should trigger the mentioned change.
+        $statusButton = $crawler->selectButton($button_text);
+        $form = $statusButton->form();
+        self::createAnonymousClient()->submit($form);
+
+        $crawler = self::createAnonymousClient()->followRedirect();
+
+        $filter_string = "div:contains('".$flash_text."')";
+        $this->assertEquals(4, $crawler->filter($filter_string)->count());
+
+        $crawler = $this->teamMemberGoTo('/kontrollpanel/opptak/fordelt');
+
+        // Verify that a change has taken place.
+        $this->assertEquals($count_no_answer - 1, $crawler->filter('td:contains("Ingen svar")')->count());
+        $this->assertEquals($count_status + 1, $crawler->filter('td:contains('.$status.')')->count());
+
+        \TestDataManager::restoreDatabase();
+    }
 }
