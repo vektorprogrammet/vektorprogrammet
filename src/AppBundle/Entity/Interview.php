ea41b0667c5d3753a59147f96802c2f57e5fd288
aestene <aestene@users.noreply.github.com>
2017-04-21 17:14:35 +0200

    Revert "Interview mail room (#495)"
    This reverts commit 7a5a13b823808c1a7b826e528d1d6d212939e748.

--- src/AppBundle/Entity/Interview.php
+++ src/AppBundle/Entity/Interview.php
@@ -35,11 +35,6 @@ class Interview
     protected $scheduled;
 
     /**
-     * @ORM\Column(type="string", nullable=true)
-     */
-    protected $room;
-
-    /**
      * @ORM\Column(type="datetime", nullable=true)
      */
     protected $conducted;
@@ -244,22 +239,6 @@ class Interview
     public function setCancelled($cancelled)
     {
         $this->cancelled = $cancelled;
-    }
-
-    /**
-     * @return string
-     */
-    public function getRoom()
-    {
-        return $this->room;
-    }
-
-    /**
-     * @param string $room
-     */
-    public function setRoom($room)
-    {
-        $this->room = $room;
     }
 
     /**
