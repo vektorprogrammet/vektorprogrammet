c8d76320fb34dbff57b964b823a74350da8a509c
Kristoffer Ravik Andresen <kristandre@users.noreply.github.com>
2017-04-21 18:26:38 +0200

    Interview mail room (#532)
    * Endret spørsmål ved søking for gamle assistenter fra "Hvilke dager passer?" til "Hvilke dager passer IKKE?"
    
    * La til boks for å legge inn rom når intervjuer skal varsle intervjuobjekt om kommende intervju.
    
    * Update config_dev.yml
    
    * Update config_dev.yml
    
    * Update config_dev.yml
    
    * Update config_dev.yml
    
    * Update config_dev.yml
    
    * La til "room" i $interview, og at det automatisk ble puttet inn ved oppsetting av intervju. Under oppsatte intervjuer, så vises rommet som intervjuet skal skje i.
    
    * Create migration script

---
+++ app/DoctrineMigrations/VersionInterviewRoom.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+class VersionInterviewRoom extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE interview ADD room VARCHAR(255) DEFAULT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE interview DROP room');
+    }
+}
