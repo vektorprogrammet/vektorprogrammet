b49f4601ed201c10d5f964761e5011c609959d08
Kristoffer <kra1992@hotmail.com>
2017-05-21 14:49:27 +0200

    Add version to all assets to bust browser cache when updated

---
+++ src/AppBundle/Twig/Extension/AssetExtension.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace AppBundle\Twig\Extension;
+
+use Symfony\Component\Asset\Packages;
+
+class AssetExtension extends \Twig_Extension {
+	private $packages;
+	private $rootDir;
+
+	/**
+	 * AssetExtension constructor.
+	 *
+	 * @param Packages $packages
+	 * @param string $rootDir
+	 */
+	public function __construct( Packages $packages, string $rootDir ) {
+		$this->packages = $packages;
+		$this->rootDir  = $rootDir;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getFunctions() {
+		return array(
+			new \Twig_SimpleFunction( 'asset_with_version', array( $this, 'getAssetUrl' ) ),
+		);
+	}
+
+	/**
+	 * Returns the public url/path of an asset.
+	 *
+	 * If the package used to generate the path is an instance of
+	 * UrlPackage, you will always get a URL and not a path.
+	 *
+	 * @param string $path A public path
+	 * @param string $packageName The name of the asset package to use
+	 *
+	 * @return string The public path of the asset
+	 */
+	public function getAssetUrl( $path, $packageName = null ) {
+		if (strlen($path) === 0) {
+			return $path;
+		}
+
+		if ( $path[0] !== '/' ) {
+			$path = "/$path";
+		}
+		$filePath = $this->rootDir . "/../www$path";
+
+		$version = "";
+		if ( file_exists( $filePath ) ) {
+			$version = filemtime( $filePath );
+		}
+
+		$url = $this->packages->getUrl( $path, $packageName );
+		if ( strlen( $version ) > 0 ) {
+			$url .= "?v=" . $version;
+		}
+
+		return $url;
+	}
+}
