8a046ec43346064be56e7618ae2dcc74939a86f7
jvals <jvals@users.noreply.github.com>
2017-05-21 20:20:32 +0200

    Get current semester instead of latest

--- src/AppBundle/Service/ApplicationData.php
+++ src/AppBundle/Service/ApplicationData.php
@@ -47,7 +47,7 @@ class ApplicationData
     public function setDepartment(Department $department)
     {
         $this->department = $department;
-        $this->semester = $this->em->getRepository('AppBundle:Semester')->findLatestSemesterByDepartmentId($department->getId());
+        $this->semester = $this->em->getRepository('AppBundle:Semester')->findCurrentSemesterByDepartment($department);
     }
 
     public function setSemester(Semester $semester)
