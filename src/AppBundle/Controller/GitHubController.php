c31bddb343223f762640abbd0067f92e814b05c9
Kristoffer <kra1992@hotmail.com>
2017-06-12 21:40:53 +0200

    Update commit message to Slack log when deploying

--- src/AppBundle/Controller/GitHubController.php
+++ src/AppBundle/Controller/GitHubController.php
@@ -31,9 +31,9 @@ class GitHubController extends Controller
 
         // Execute deploy script if there is a push to master
         if ($isCorrectRepository && $isMaster && $commit !== null) {
-            $this->get('app.logger')->info("New commit on master by $committer:");
-            $this->get('app.logger')->info($message);
-            $this->get('app.logger')->info('Starting to deploy changes from master branch...');
+            $this->get('app.logger')->info("New commit on master by *$committer*:");
+            $this->get('app.logger')->info("```$message```");
+            $this->get('app.logger')->info('Deploying changes...');
             shell_exec($this->getParameter('kernel.root_dir').'/../deploy.sh');
             $this->get('app.logger')->info('Deploy complete');
 
