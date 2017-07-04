d591f60ad7d7dcc04af0457ac184f49ecea06418
Kristoffer <kra1992@hotmail.com>
2017-07-04 11:26:38 +0200

    Improve schedule validation

--- src/AppBundle/SchoolAllocation/Webapp/src/Schedule.js
+++ src/AppBundle/SchoolAllocation/Webapp/src/Schedule.js
@@ -1,5 +1,5 @@
 function randomElement(arr) {
-  const index = Math.floor(Math.random()*arr.length);
+  const index = Math.floor(Math.random() * arr.length);
 
   return arr[index];
 }
@@ -63,11 +63,11 @@ export class Schedule {
     if (!this.hasOwnProperty(day)) return false;
     if (!this.hasOwnProperty(assistantsKey)) return false;
 
-      this[assistantsKey].push(assistant);
-      this._removeAssistantFromList(assistant, this.queuedAssistants);
+    this[assistantsKey].push(assistant);
+    this._removeAssistantFromList(assistant, this.queuedAssistants);
 
-      busyAssistants.push(assistant);
-      assistant.assignedDay = assistantsKey;
+    busyAssistants.push(assistant);
+    assistant.assignedDay = assistantsKey;
   }
 
   _removeAssistantFromList(assistant, list) {
@@ -82,12 +82,12 @@ export class Schedule {
     const assistantsKeyTo = toDay + "AssistantsGroup" + toGroup;
     this[assistantsKeyTo].push(assistant);
     this._removeAssistantFromList(assistant, this[assistantsKeyFrom]);
-      assistant.assignedDay = assistantsKeyTo;
+    assistant.assignedDay = assistantsKeyTo;
   }
 
   deepCopy() {
     const clone = new Schedule(this.schools, this._copy(this.queuedAssistants));
-    for (let i = 0; i < weekDays.length; i++){
+    for (let i = 0; i < weekDays.length; i++) {
       const day = weekDays[i];
       clone[day + "AssistantsGroup1"] = this._copy(this[day + "AssistantsGroup1"]);
       clone[day + "AssistantsGroup2"] = this._copy(this[day + "AssistantsGroup2"]);
@@ -108,13 +108,13 @@ export class Schedule {
 
   totalCapacity() {
     return weekDays.reduce((acc, day) => (
-        acc + this[day] * 2 // Group 1 & group 2
+      acc + this[day] * 2 // Group 1 & group 2
     ), 0);
   }
 
   scheduledAssistantsCount() {
     let count = 0;
-    for (let i = 0; i < weekDays.length; i++){
+    for (let i = 0; i < weekDays.length; i++) {
       const day = weekDays[i];
       count += this[day + "AssistantsGroup1"].length;
       count += this[day + "AssistantsGroup2"].length;
@@ -130,120 +130,156 @@ export class Schedule {
     for (let group = 1; group <= 2; group++) {
       for (let i = 0; i < weekDays.length; i++) {
         const day = weekDays[i];
-        const assistantsKey = day + "AssistantsGroup" + group;
-        const assistants = this[assistantsKey];
+        const assistants = this.getAssignedAssistants(day, group);
+
         for (let j = 0; j < assistants.length; j++) {
           const assistant = assistants[j];
-          if (assistant.preferredGroup !== null && assistant.preferredGroup !== group) {
+
+          const inNonPreferredGroup = assistant.preferredGroup !== null && assistant.preferredGroup !== group;
+          const assignedToMultipleDays = !this._assistantAssignedToOnlyOneDay(assistant);
+          const overBooked = !assistant.double && this._assistantHasDoublePosition(assistant, day);
+
+          if (inNonPreferredGroup || assignedToMultipleDays || overBooked) {
             return false;
           }
         }
+
       }
     }
 
     return true;
   }
 
-  mutate() {
-    const clone = this.deepCopy();
-    const group = randomElement([1, 2]);
-    const fromDay = randomElement(weekDays);
-    const assistantsKey = fromDay + "AssistantsGroup" + group;
-    const assistants = this[assistantsKey];
+  _assistantAssignedToOnlyOneDay(assistant) {
+    const assignedDays = [];
 
-    const length = assistants.length;
-    if (length === 0) {
-      return clone;
-    }
-    const assistant = randomElement(assistants);
-    const availableDays = weekDays.filter(weekday => assistant[weekday] && fromDay !== weekday);
-    if (availableDays.length === 0) {
-      return clone;
-    }
-    const double = assistant.double;
-    const toDay = randomElement(availableDays);
-    if (double) {
-      if (this.hasCapacity(toDay, 1) && (this.hasCapacity(toDay, 2))) {
-        clone.reassignAssistant(assistant, fromDay, toDay, 1, 1);
-        clone.reassignAssistant(assistant, fromDay, toDay, 2, 2);
-        return clone;
-      }
-    } else if (assistant.preferredGroup === group){
-      if (this.hasCapacity(toDay, group)) {
-        clone.reassignAssistant(assistant, fromDay, toDay, group, group);
-        return clone;
-      }
-    } else if (assistant.preferredGroup === null) {
-      const randomGroup = randomElement([1, 2]);
-      if (this.hasCapacity(toDay, randomGroup)) {
-        clone.reassignAssistant(assistant, fromDay, toDay, group, randomGroup);
-        return clone;
+    for (let group = 1; group <= 2; group++) {
+      for (let i = 0; i < weekDays.length; i++) {
+        const day = weekDays[i];
+        const assistants = this.getAssignedAssistants(day, group);
+
+        if (assistants.indexOf(assistant) !== -1) {
+          if (assignedDays.indexOf(day) === -1) {
+            assignedDays.push(day);
+          }
+        }
+
       }
     }
 
-    return clone;
+    return assignedDays.length === 1;
   }
 
-    mutate2() {
-	const clone = this.deepCopy();
-	const remainingDays = this.getRemainingDays();
-	if (remainingDays.length === 0) {
-	    return clone;
-	}
-	const toDayKey = randomElement(Object.keys(remainingDays));
-	const toDay = toDayKey.slice(0, -16);
-	const toGroup = toDayKey.substr(toDayKey.length - 1);
-	for (let i = 0; i < busyAssistants.length; i++) {
-	    const busyAssistant = busyAssistants[i];
-	    const double = busyAssistant.double;
-	    if (busyAssistant[toDay]) {
-		if (double) {
-		    if (this.hasCapacity(toDay, 1) && this.hasCapacity(toDay, 2)) {
-			const fromDayKey = busyAssistant.assignedDay;
-			const fromGroup = fromDayKey.substr(fromDayKey.length -1 );
-			const fromDay = fromDayKey.slice(0, -16);
-			clone.reassignAssistant(busyAssistant, fromDay, toDay, 1, 1);
-			clone.reassignAssistant(busyAssistant, fromDay, toDay, 2, 2);
-			break;
-		    }
-		} else if (busyAssistant.preferredGroup === toGroup){
-		    const fromDayKey = busyAssistant.assignedDay;
-		    const fromGroup = fromDayKey.substr(fromDayKey.length -1 );
-		    const fromDay = fromDayKey.slice(0, -16);
-		    clone.reassignAssistant(busyAssistant, fromDay, toDay, fromGroup, toGroup);
-		    break;
-		} else if (busyAssistant.preferredGroup !== toGroup){
-		    continue;
-		} else { // Not double, any group
-		    const fromDayKey = busyAssistant.assignedDay;
-		    const fromGroup = fromDayKey.substr(fromDayKey.length -1 );
-		    const fromDay = fromDayKey.slice(0, -16);
-		    clone.reassignAssistant(busyAssistant, fromDay, toDay, fromGroup, toGroup);
-		    break;
-		}
-	    }
-	}
-	return clone;
+  _assistantHasDoublePosition(assistant, day) {
+    const assistantsInGroup1 = this.getAssignedAssistants(day, 1);
+    const assistantsInGroup2 = this.getAssignedAssistants(day, 2);
 
+    return assistantsInGroup1.indexOf(assistant) !== -1 && assistantsInGroup2.indexOf(assistant) !== -1;
+  }
+
+mutate()
+{
+  const clone = this.deepCopy();
+  const group = randomElement([1, 2]);
+  const fromDay = randomElement(weekDays);
+  const assistantsKey = fromDay + "AssistantsGroup" + group;
+  const assistants = this[assistantsKey];
+
+  const length = assistants.length;
+  if (length === 0) {
+    return clone;
+  }
+  const assistant = randomElement(assistants);
+  const availableDays = weekDays.filter(weekday => assistant[weekday] && fromDay !== weekday);
+  if (availableDays.length === 0) {
+    return clone;
+  }
+  const double = assistant.double;
+  const toDay = randomElement(availableDays);
+  if (double) {
+    if (this.hasCapacity(toDay, 1) && (this.hasCapacity(toDay, 2))) {
+      clone.reassignAssistant(assistant, fromDay, toDay, 1, 1);
+      clone.reassignAssistant(assistant, fromDay, toDay, 2, 2);
+      return clone;
+    }
+  } else if (assistant.preferredGroup === group) {
+    if (this.hasCapacity(toDay, group)) {
+      clone.reassignAssistant(assistant, fromDay, toDay, group, group);
+      return clone;
+    }
+  } else if (assistant.preferredGroup === null) {
+    const randomGroup = randomElement([1, 2]);
+    if (this.hasCapacity(toDay, randomGroup)) {
+      clone.reassignAssistant(assistant, fromDay, toDay, group, randomGroup);
+      return clone;
+    }
+  }
+
+  return clone;
+}
+
+mutate2()
+{
+  const clone = this.deepCopy();
+  const remainingDays = this.getRemainingDays();
+  if (remainingDays.length === 0) {
+    return clone;
+  }
+  const toDayKey = randomElement(Object.keys(remainingDays));
+  const toDay = toDayKey.slice(0, -16);
+  const toGroup = toDayKey.substr(toDayKey.length - 1);
+  for (let i = 0; i < busyAssistants.length; i++) {
+    const busyAssistant = busyAssistants[i];
+    const double = busyAssistant.double;
+    if (busyAssistant[toDay]) {
+      if (double) {
+        if (this.hasCapacity(toDay, 1) && this.hasCapacity(toDay, 2)) {
+          const fromDayKey = busyAssistant.assignedDay;
+          const fromGroup = fromDayKey.substr(fromDayKey.length - 1);
+          const fromDay = fromDayKey.slice(0, -16);
+          clone.reassignAssistant(busyAssistant, fromDay, toDay, 1, 1);
+          clone.reassignAssistant(busyAssistant, fromDay, toDay, 2, 2);
+          break;
+        }
+      } else if (busyAssistant.preferredGroup === toGroup) {
+        const fromDayKey = busyAssistant.assignedDay;
+        const fromGroup = fromDayKey.substr(fromDayKey.length - 1);
+        const fromDay = fromDayKey.slice(0, -16);
+        clone.reassignAssistant(busyAssistant, fromDay, toDay, fromGroup, toGroup);
+        break;
+      } else if (busyAssistant.preferredGroup !== toGroup) {
+        continue;
+      } else { // Not double, any group
+        const fromDayKey = busyAssistant.assignedDay;
+        const fromGroup = fromDayKey.substr(fromDayKey.length - 1);
+        const fromDay = fromDayKey.slice(0, -16);
+        clone.reassignAssistant(busyAssistant, fromDay, toDay, fromGroup, toGroup);
+        break;
+      }
+    }
+  }
+  return clone;
+
+}
+
+getRemainingDays()
+{
+  let remainingDays = {};
+
+  for (let i = 0; i < weekDays.length; i++) {
+    const day = weekDays[i];
+    const remaining1 = this[day] - this[day + "AssistantsGroup1"].length;
+    const remaining2 = this[day] - this[day + "AssistantsGroup2"].length;
+
+    if (remaining1 > 0) {
+      remainingDays[day + "AssistantsGroup1"] = remaining1;
     }
 
-    getRemainingDays() {
-	let remainingDays = {};
-
-	for (let i = 0; i < weekDays.length; i++) {
-	    const day = weekDays[i];
-	    const remaining1 = this[day] - this[day + "AssistantsGroup1"].length;
-	    const remaining2 = this[day] - this[day + "AssistantsGroup2"].length;
-
-	    if (remaining1 > 0) {
-		remainingDays[day + "AssistantsGroup1"] = remaining1;
-	    }
-
-	    if (remaining2 > 0) {
-		remainingDays[day + "AssistantsGroup2"] = remaining2;
-	    }
-	}
-
-	return remainingDays;
+    if (remaining2 > 0) {
+      remainingDays[day + "AssistantsGroup2"] = remaining2;
     }
+  }
+
+  return remainingDays;
+}
 }
