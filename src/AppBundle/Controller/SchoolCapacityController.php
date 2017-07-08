f277180dca3eda490cd68e5af0d422d32f76e932
Kristoffer <kra1992@hotmail.com>
2017-07-08 12:18:09 +0200

    Clean up controllers

--- src/AppBundle/Controller/SchoolCapacityController.php
+++ src/AppBundle/Controller/SchoolCapacityController.php
@@ -3,39 +3,54 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\SchoolCapacity;
+use AppBundle\Form\Type\SchoolCapacityEditType;
 use AppBundle\Form\Type\SchoolCapacityType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 class SchoolCapacityController extends Controller
 {
-    public function createAction(Request $request)
-    {
-	    $user = $this->getUser();
-	    $department = $user->getDepartment();
-	    $currentSemester = $this->getDoctrine()->getRepository('AppBundle:Semester')->findCurrentSemesterByDepartment($department);
+	public function createAction(Request $request)
+	{
+		$user = $this->getUser();
+		$department = $user->getDepartment();
+		$currentSemester = $this->getDoctrine()->getRepository('AppBundle:Semester')->findCurrentSemesterByDepartment($department);
 
-	    $schoolCapacity = new SchoolCapacity();
-	    $schoolCapacity->setSemester($currentSemester);
-	    $form = $this->createForm(new SchoolCapacityType(), $schoolCapacity);
-	    $form->handleRequest($request);
+		$schoolCapacity = new SchoolCapacity();
+		$schoolCapacity->setSemester($currentSemester);
+		$form = $this->createForm(new SchoolCapacityType(), $schoolCapacity);
+		$form->handleRequest($request);
 
-	    if ($form->isValid()) {
-//		    return $this->render('school_admin/school_allocate_create.html.twig', array(
-//			    'message' => 'Skolen eksisterer allerede',
-//			    'form' => $form->createView(),
-//		    ));
+		if ($form->isValid()) {
+			$em = $this->getDoctrine()->getManager();
+			$em->persist($schoolCapacity);
+			$em->flush();
 
-		    $em = $this->getDoctrine()->getManager();
-		    $em->persist($schoolCapacity);
-		    $em->flush();
+			return $this->redirect($this->generateUrl('school_allocation'));
+		}
 
-		    return $this->redirect($this->generateUrl('school_allocation'));
-	    }
+		return $this->render('school_admin/school_allocate_create.html.twig', array(
+			'message' => '',
+			'form' => $form->createView(),
+		));
+	}
 
-	    return $this->render('school_admin/school_allocate_create.html.twig', array(
-		    'message' => '',
-		    'form' => $form->createView(),
-	    ));
-    }
+	public function editAction(Request $request, SchoolCapacity $capacity)
+	{
+		$form = $this->createForm(new SchoolCapacityEditType(), $capacity);
+		$form->handleRequest($request);
+
+		if ($form->isValid()) {
+			$em = $this->getDoctrine()->getManager();
+			$em->persist($capacity);
+			$em->flush();
+
+			return $this->redirect($this->generateUrl('school_allocation'));
+		}
+
+		return $this->render('school_admin/school_allocate_edit.html.twig', array(
+			'capacity' => $capacity,
+			'form' => $form->createView(),
+		));
+	}
 }
