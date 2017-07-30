9d46c1a8fca63a4990dad669d337796bc53b858f
sigtot <sigurdtotland@gmail.com>
2017-07-30 19:04:37 +0200

    More realistic datafixtures with images

--- src/AppBundle/DataFixtures/ORM/LoadReceiptData.php
+++ src/AppBundle/DataFixtures/ORM/LoadReceiptData.php
@@ -15,40 +15,44 @@ class LoadReceiptData extends AbstractFixture implements OrderedFixtureInterface
         $receipt1->setUser($this->getReference('user-1'));
         $receipt1->setSubmitDate(new \DateTime());
         $receipt1->setDescription(
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+            'Kaffetraktere og grenuttak til stand'
         );
-        $receipt1->setSum(120.50);
+        $receipt1->setSum(1108);
         $receipt1->setActive(true);
+        $receipt1->setPicturePath('/images/receipt_images/clas.jpg');
         $manager->persist($receipt1);
 
         $receipt2 = new Receipt();
         $receipt2->setUser($this->getReference('user-1'));
         $receipt2->setSubmitDate(new \DateTime());
         $receipt2->setDescription(
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+            'Taco til Tor'
         );
-        $receipt2->setSum(100);
+        $receipt2->setSum(133);
         $receipt2->setActive(true);
+        $receipt2->setPicturePath('/images/receipt_images/taco.jpg');
         $manager->persist($receipt2);
 
         $receipt3 = new Receipt();
         $receipt3->setUser($this->getReference('user-1'));
         $receipt3->setSubmitDate(new \DateTime());
         $receipt3->setDescription(
-            'Rekrutteringsfylla'
+            'Teamsosialt med IT'
         );
-        $receipt3->setSum(7000);
+        $receipt3->setSum(531);
         $receipt3->setActive(false);
+        $receipt3->setPicturePath('/images/receipt_images/teamsosialt.jpg');
         $manager->persist($receipt3);
 
         $receipt4 = new Receipt();
         $receipt4->setUser($this->getReference('user-2'));
         $receipt4->setSubmitDate(new \DateTime());
         $receipt4->setDescription(
-            'Vafler på kontoret'
+            'Innkjøp til hyttetur'
         );
-        $receipt4->setSum(130.40);
+        $receipt4->setSum(828.77);
         $receipt4->setActive(true);
+        $receipt4->setPicturePath('/images/receipt_images/hyttetur.jpg');
         $manager->persist($receipt4);
 
         $manager->flush();
