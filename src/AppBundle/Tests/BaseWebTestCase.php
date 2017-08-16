d35a5950a79c95b354dc39aeb3170a2b783b4568
Kristoffer <kra1992@hotmail.com>
2017-08-16 18:31:55 +0200

    Restore database after every test

--- src/AppBundle/Tests/BaseWebTestCase.php
+++ src/AppBundle/Tests/BaseWebTestCase.php
@@ -119,4 +119,10 @@ abstract class BaseWebTestCase extends WebTestCase
 
         return $crawler->filter('tr')->count();
     }
+
+    protected function tearDown() {
+	    parent::tearDown();
+
+	    \TestDataManager::restoreDatabase();
+    }
 }
