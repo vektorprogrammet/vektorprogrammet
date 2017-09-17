e3eb08cbbffef7b5d1e35ee730664130c1c8cea8
Kristoffer <kra1992@hotmail.com>
2017-09-17 22:14:59 +0200

    Update Header

--- client/src/containers/Header.js
+++ client/src/containers/Header.js
@@ -1,14 +1,28 @@
 import React, {Component} from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { Grid, Image } from 'semantic-ui-react';
+import logo from '../images/logo_condensed.png';
+import './Header.css';
 
 class Header extends Component {
   render() {
     return (
-        <nav>
-          <Link to={'/'}>Hjem</Link>
+    <Grid>
+      <Grid.Row centered>
+        <Link to={'/'}>
+          <Image className="logo" src={logo} />
+        </Link>
+      </Grid.Row>
+      <Grid.Row centered>
+        <nav className="main-navigation">
           <Link to={'/assistenter'}>Assistenter</Link>
           <Link to={'/team'}>Team</Link>
+          <Link to={'/laerere'}>Lærere</Link>
+          <Link to={'/foreldre'}>Foreldre</Link>
+          <Link to={'/om-oss'}>Om oss</Link>
         </nav>
+      </Grid.Row>
+    </Grid>
     );
   }
 }
