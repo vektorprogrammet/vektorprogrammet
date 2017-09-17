28cf69998b9e7e2c07cfd94513cedead97bc62d0
Kristoffer <kra1992@hotmail.com>
2017-09-17 15:31:16 +0200

    Create SponsorList tests

--- client/src/components/SponsorList.js
+++ client/src/components/SponsorList.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import './SponsorList.css';
 
-export class SponsorList extends Component {
+export default class SponsorList extends Component {
   render() {
-    const sponsors = this.props.sponsors.map(sponsor => (
+    const sponsors = this.props.sponsors || [];
+    const sponsorItems = sponsors.map(sponsor => (
         <li key={sponsor.id}><a href={sponsor.url}>{sponsor.name}</a></li>
     ));
 
     return (
-        <ul>{sponsors}</ul>
+        <ul>{sponsorItems}</ul>
     );
   }
 }
