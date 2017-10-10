e9b18908bea79047d8749d37b26402e4675252d2
Kongen Duc <tamduchavo@hotmail.com>
2017-10-10 18:24:34 +0200

    Created main text-component, list-component for the buttons on the right side

--- client/src/components/ContactInformation.js
+++ client/src/components/ContactInformation.js
@@ -1,31 +1,29 @@
 import React, {Component} from 'react';
 import './ContactInformation.css';
 
-class ContactInfo extends Component {
-    render() {
-        return (
-            <section>
-                /*TODO: Move the styling to css-file*/
-                <ul className="unordered-list">
-                    <li className="list-container">
-                        Kontakt:
-                        <li className="list-ntnu">
-                            <a href="mailto:NTNU@gmail.com">Trondheim - NTNU</a>
-                        </li>
-                        <li className="list-hist">
-                            <a href="mailto:HIST@gmail.com">Trondheim - HIST</a>
-                        </li>
-                        <li className="list-nmbu">
-                            <a href="mailto:NMBU@gmail.com">Ås</a>
-                        </li>
-                        <li  className="list-uio">
-                            <a href="mailto:UiO@gmail.com">Oslo</a>
-                        </li>
+export const ContactInfo = () => {
+    return (
+        <section>
+            /*TODO: Move the styling to css-file*/
+            <ul className="unordered-list">
+                <li className="list-container">
+                    Kontakt:
+                    <li className="list-ntnu">
+                        <a href="mailto:NTNU@gmail.com">Trondheim - NTNU</a>
                     </li>
-                </ul>
-            </section>
-        );
-    }
-}
+                    <li className="list-hist">
+                        <a href="mailto:HIST@gmail.com">Trondheim - HIST</a>
+                    </li>
+                    <li className="list-nmbu">
+                        <a href="mailto:NMBU@gmail.com">Ås</a>
+                    </li>
+                    <li  className="list-uio">
+                        <a href="mailto:UiO@gmail.com">Oslo</a>
+                    </li>
+                </li>
+            </ul>
+        </section>
+    );
+};
 
 export default ContactInfo;
--- client/src/containers/AboutUsPage.js
+++ client/src/containers/AboutUsPage.js
@@ -1,16 +1,17 @@
 /*TODO: It is not possible to access the other pages from this page, find out why*/
 import React, {Component} from 'react';
-import { Image, Grid, Divider, List, Button} from 'semantic-ui-react';
+import {Grid, Divider} from 'semantic-ui-react';
 import './AboutUsPage.css';
 import ContactUsPopUp from '../components/ContactUsPopUp';
 import Faq from '../components/Faq';
 import ContactInformation from '../components/ContactInformation';
+import ListComponentAboutUs from '../components/ListComponentAboutUs';
+import MainTextAboutUs from '../components/MainTextAboutUs.js';
 
 class AboutUsPage extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-
-        this.state={
+        this.state = {
             contactUsVisible: false,
             contactUsRevealed: false
         };
@@ -23,11 +24,12 @@ class AboutUsPage extends Component {
             contactUsVisible : false
         });
     }
+
     handleContactUsNowClick(){
         this.setState ({
-                contactUsVisible: true,
-                contactUsRevealed: true
-            });
+            contactUsVisible: true,
+            contactUsRevealed: true
+        });
     }
 
     render() {
@@ -53,66 +55,30 @@ class AboutUsPage extends Component {
 
         return (
             <Grid columns={3}>
-                <ContactUsPopUp show={this.state.contactUsVisible} onClose={this.onContactUsPopUpClose}/>
                 <Grid.Column width={2}>
                 </Grid.Column>
                 <Grid.Column width={10}>
-                        <Grid>
-                            <Grid.Row>
-                                <h1 className="aboutUs-header">Om Vektorprogrammet</h1>
-                            </Grid.Row>
-                            <Grid.Row columns={2}>
-                                <Grid.Column>
-                                    <div>
-                                        <p className="aboutUs-text">
-                                            Vektorprogrammet arbeider for å øke interessen for matematikk og realfag blant elever i grunnskolen. Vi er en nasjonal studentorganisasjon som sender studenter med utmerket
-                                            realfagskompetanse til skoler for å hjelpe elevene i matematikktimene. Disse studentene har også gode pedagogiske evner og er gode rollemodeller – de er Norges realfagshelter.
-                                            <br/><br/>
-                                        </p>
-                                    </div>
-                                </Grid.Column>
-                                <Grid.Column>
-                                    <Image className="aboutUs-image1" src={"http://via.placeholder.com/350x350/"}/>
-                                </Grid.Column>
-                            </Grid.Row>
-                            <Grid.Row columns={2}>
-                                <Grid.Column>
-                                    <Image className="aboutUs-image2" src={"http://via.placeholder.com/350x350/"}/>
-                                </Grid.Column>
-                                <Grid.Column>
-                                    <div>
-                                        <p className="aboutUs-text2">
-                                            Da studentene er tilstede i skoletiden skiller vi oss derfor fra andre tiltak ved at elevenes barriere for å delta er ikke-eksisterende. Dette gjør at vi når ut til alle –
-                                            ikke kun de som allerede er motiverte. <br/><br/>Vektorprogrammet sørger for at alle får hjelp i timen raskere og at undervisningen kan bli mer tilpasset de ulike elevgruppene.
-                                        </p>
-                                    </div>
-                                </Grid.Column>
-                            </Grid.Row>
-                            <Divider fitted/>
-                            <Grid.Row>
-                                <div id={"FAQ"} className="aboutUs-FAQ">
-                                    <Faq questionsAndAnswers={faqQuestionsAndAnswers}/>
-                                </div>
-                            </Grid.Row>
-                            <Divider fitted/>
-                            <Grid.Row>
-                                <div id={"kontakt-info"} className="contact-info">
-                                    <ContactInformation/>
-                                </div>
-                            </Grid.Row>
-                        </Grid>
+                    <Grid>
+                        <MainTextAboutUs/>
+                        <Divider fitted/>
+                        <Grid.Row>
+                            <div id={"FAQ"} className="aboutUs-FAQ">
+                                <Faq questionsAndAnswers={faqQuestionsAndAnswers}/>
+                            </div>
+                        </Grid.Row>
+                        <Divider fitted/>
+                        <div className="fix" style={{marginTop: 500}}>
+                            <ContactUsPopUp show={this.state.contactUsVisible} onClose={this.onContactUsPopUpClose}/>
+                        </div>
+                        <Grid.Row>
+                            <div id={"kontakt-info"} className="contact-info">
+                                <ContactInformation/>
+                            </div>
+                        </Grid.Row>
+                    </Grid>
                 </Grid.Column>
                 <Grid.Column width={4}>
-                    <List size={"massive"}>
-                        <List.Item icon='question circle' content={<a href={'#FAQ'}>FAQ</a>} />
-                        <List.Item icon='envelope' content={<a href={'#kontakt-info'}>Kontaktinformasjon</a>} />
-                        <List.Item
-                            icon='talk'
-                            content={
-                                <Button basic color='blue' onClick={this.handleContactUsNowClick}>Kontakt oss nå!</Button>
-                            }
-                        />
-                    </List>
+                    <ListComponentAboutUs show ={this.handleContactUsNowClick}/>
                 </Grid.Column>
             </Grid>
         )
