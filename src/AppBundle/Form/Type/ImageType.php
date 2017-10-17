a2e4c2a1b2b7515bbf84e028d55ff2719c83321c
sigtot <sigurdtotland@gmail.com>
2017-10-17 19:10:56 +0200

    Edit description functionality

---
+++ src/AppBundle/Form/Type/ImageType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AppBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ImageType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('description', TextType::class, array(
+            'label' => 'Beskrivelse',
+            'required' => true,
+        ));
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Image',
+        ));
+    }
+
+    public function getName()
+    {
+        return 'imageDescription';
+    }
+}
