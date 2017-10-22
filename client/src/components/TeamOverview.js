cd3a11210813419bdd741e8297d2d53b57159281
jvals <jvals@users.noreply.github.com>
2017-10-22 15:05:10 +0200

    Simplify teamOverview component

--- client/src/components/TeamOverview.js
+++ client/src/components/TeamOverview.js
@@ -2,73 +2,43 @@ import React, {Component} from 'react';
 import {Icon, Header, Menu, Segment, Button, Card, Image, List, Label, Tab} from 'semantic-ui-react';
 import './TeamOverview.css';
 import TeamCard from './TeamCard';
-import {TeamApi} from '../api/TeamApi.js';
 import {DepartmentApi} from '../api/DepartmentApi.js';
 
 export default class TeamOverview extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            teams: new Map(),
-            departments: [],
-            cardGroup: new Map(),
-            menuItems: []
-        };
-
-        this.getCard = this.getCard.bind(this);
-
-    }
-
-    componentDidMount() {
-        const newTeams = new Map();
-        const newCardGroup = new Map();
-        DepartmentApi.getAll().then(json => {
-            this.setState({departments: json})
-        }).then(() => {
-            const cls = this;
-            this.state.departments.forEach(function (dep) {
-                DepartmentApi.getTeams(dep.id).then(json => {
-                    newTeams.set(dep.id, json);
-                    newCardGroup.set(dep.id, cls.getCard(dep.id))
-                })
-            });
-            this.setState(
-                {teams: newTeams}
-            );
-            this.setState(
-                {cardGroup: newCardGroup}
+    getPanes(departments) {
+        const panes = [];
+        departments.forEach(function (department) {
+            const teams = department.teams;
+            const teamCards = teams.map((team) =>
+                <TeamCard
+                    key={team.name}
+                    name={team.name}
+                    email={team.email}
+                    shortDesc={team.short_description}
+                    accept_application={team.accept_application}
+                />
             );
 
-            const menuItems = this.state.departments.map((dep) => {
-                const item = {
-                    menuItem: dep.city,
-                    render: () => <Tab.Pane className="tabPaneContent">
-                        <Card.Group className="teamCardGroup">
-                            {this.state.cardGroup.get(dep.id)}
-                        </Card.Group>
+            const cardGroup = (
+                <Card.Group className="teamCardGroup">
+                    {teamCards}
+                </Card.Group>
+            );
+
+            const pane = {
+                menuItem: department.city,
+                render: () =>
+                    <Tab.Pane className="tabPaneContent">
+                        {cardGroup}
                     </Tab.Pane>
-                };
-                return item;
-            });
+            };
 
-            this.setState(
-                {menuItems: menuItems}
-            )
-        })
-    }
+            panes.push(pane);
 
-    getCard(department) {
-        const teams = this.state.teams.get(department);
-        const cardGroup = teams.map((team) =>
-            <TeamCard key={team.name}
-                      name={team.name}
-                      email={team.email}
-                      shortDesc={team.short_description}
-                      accept_application={team.accept_application}
-            />
-        );
-        return cardGroup;
+        }.bind(this));
+
+        return panes;
     }
 
     render() {
@@ -82,7 +52,7 @@ export default class TeamOverview extends Component {
                     attached: false,
                     secondary: true,
                     pointing: false
-                }} panes={this.state.menuItems}/>
+                }} panes={this.getPanes()}/>
             </Segment>
         );
     }
