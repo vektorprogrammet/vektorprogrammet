33751421dcfb9f26583fc145e8dd621104d2c2c6
kristandre <kra1992@hotmail.com>
2017-10-31 19:24:39 +0100

    Create login page

---
+++ client/src/containers/LoginPage.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './LoginPage.css';
+
+class LoginPage extends Component {
+  render() {
+    return (
+      <p>This is the login page!</p>
+    );
+  }
+}
+
+export default LoginPage;
