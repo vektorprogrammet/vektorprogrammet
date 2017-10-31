07952671ee0fa4c1446c1dddb2d304b31df78fb0
Kongen Duc <tamduchavo@hotmail.com>
2017-10-31 19:44:18 +0100

    Added department component to ContactPage

--- client/src/containers/ContactPage.js
+++ client/src/containers/ContactPage.js
@@ -1,9 +1,31 @@
 import React, {Component} from 'react';
 import './HomePage.css';
+import './ContactPage.css';
+import ChooseDepartment from "../components/ChooseDepartment";
+import {Grid} from 'semantic-ui-react';
 
 class ContactPage extends Component {
   render() {
-    return (<p>Hallo</p>);
+    return (
+        <div className="contact-page">
+            <Grid stackable>
+                <Grid.Row columns={4} style={{paddingLeft: 100, paddingRight: 100}}>
+                    <Grid.Column style={{paddingLeft: 50, paddingRight: 25}}>
+                        <ChooseDepartment departmentName={"NTNU"}/>
+                    </Grid.Column>
+                    <Grid.Column style={{paddingLeft: 50, paddingRight: 25}}>
+                        <ChooseDepartment departmentName={"HiST"}/>
+                    </Grid.Column>
+                    <Grid.Column style={{paddingLeft: 50, paddingRight: 25}}>
+                        <ChooseDepartment departmentName={"NMBU"}/>
+                    </Grid.Column>
+                    <Grid.Column style={{paddingLeft: 50, paddingRight: 25}}>
+                        <ChooseDepartment departmentName={"Ås"}/>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        </div>
+    );
   }
 }
 
