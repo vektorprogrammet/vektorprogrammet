d28fb75764ea0aedcf7b1dc0b3c843c64067021d
Simon Iversen <simon_iversen93@hotmail.com>
2017-10-31 18:19:40 +0100

    some shit

--- client/src/components/ContactInformation.js
+++ client/src/components/ContactInformation.js
@@ -1,11 +1,22 @@
 import './ContactInformation.css';
 import React, {Component} from 'react';
+import {Grid} from 'semantic-ui-react';
 
 // TODO: Fix styling, didn't work after implementing map function
-class ContactInfo extends Component {
+class ContactInformation extends Component {
     render() {
-
+        return (
+            <Grid>
+                <Grid.Row>
+                    <h1>Kontakt info</h1>
+                </Grid.Row>
+                <Grid.Row>
+                    <h2>E-mail</h2>
+                    <p>af</p>
+                </Grid.Row>
+            </Grid>
+        );
     }
 }
 
-export default ContactInfo;
+export default ContactInformation;
--- client/src/containers/ContactPage.js
+++ client/src/containers/ContactPage.js
@@ -1,9 +1,23 @@
 import React, {Component} from 'react';
 import './HomePage.css';
 import {Grid} from 'semantic-ui-react';
+import ContactInformation from '../components/ContactInformation';
 
 class ContactPage extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            department: 'NTNU'
+        };
+    }
+
   render() {
+    const contactSchoolInfo = [
+        {place: "Trondheim - NTNU", email: "NTNU@gmail.com"},
+        {school: "HiST", place: "Trondheim - HiST", email: "HIST@gmail.com"},
+        {school: "NMBU", place: "Ås", email: "NMBU@gmail.com"},
+        {school: "UiO", place: "Oslo", email: "UiO@gmail.com"}
+    ];
     return (
         <div className="about-us-page">
           <p>This is the contact page!</p>
@@ -13,7 +27,7 @@ class ContactPage extends Component {
                 <p>Form goes here</p>
               </Grid.Column>
               <Grid.Column width={8}>
-                <p>Contact info goes here</p>
+                < ContactInformation />
               </Grid.Column>
             </Grid.Row>
           </Grid>
