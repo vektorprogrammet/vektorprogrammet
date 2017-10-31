acb89c6f2931d10d56e201be37193e743b4e76c8
Kongen Duc <tamduchavo@hotmail.com>
2017-10-31 18:11:40 +0100

    Added form and some shit

--- client/src/containers/ContactPage.js
+++ client/src/containers/ContactPage.js
@@ -1,10 +1,35 @@
 import React, {Component} from 'react';
 import './HomePage.css';
+import './ContactPage.css';
+import {Grid} from 'semantic-ui-react';
+import ContactUsForm from "../components/ContactUsForm";
 
 class ContactPage extends Component {
   render() {
     return (
-        <p>This is the contact page!</p>
+        <div className="contact-page">
+            <Grid centered>
+                <Grid.Row>
+                    <Grid.Column width={9}>
+                        <h1>Kontakt oss</h1>
+                        <p>Før du tar kontakt med oss, må du huske å sjekke ut <a href={"om-oss#FAQ"}>ofte stilte spørsmål!</a></p>
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row columns={2}>
+                    <Grid.Column width={5}>
+                        <ContactUsForm/>
+                    </Grid.Column>
+                    <Grid.Column width={8}>
+                        <Grid.Row>
+                            <p>Contact info</p>
+                        </Grid.Row>
+                        <Grid.Row>
+                            <p>Map</p>
+                        </Grid.Row>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        </div>
     );
   }
 }
