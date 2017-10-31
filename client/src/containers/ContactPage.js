201330ae2a6acf48076dcfc1bf32543571b7b420
Simon Iversen <simon_iversen93@hotmail.com>
2017-10-31 17:27:34 +0100

    made the grid

--- client/src/containers/ContactPage.js
+++ client/src/containers/ContactPage.js
@@ -1,10 +1,23 @@
 import React, {Component} from 'react';
 import './HomePage.css';
+import {Grid} from 'semantic-ui-react';
 
 class ContactPage extends Component {
   render() {
     return (
-        <p>This is the contact page!</p>
+        <div className="about-us-page">
+          <p>This is the contact page!</p>
+          <Grid>
+            <Grid.Row>
+              <Grid.Column width={8}>
+                <p>Form goes here</p>
+              </Grid.Column>
+              <Grid.Column width={8}>
+                <p>Contact info goes here</p>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </div>
     );
   }
 }
