2df082f07c6e80847462739d4848e2bf11994f7d
Kongen Duc <tamduchavo@hotmail.com>
2017-10-31 18:32:57 +0100

    Created department component

--- client/src/components/ContactInformation.js
+++ client/src/components/ContactInformation.js
@@ -4,7 +4,7 @@ import React, {Component} from 'react';
 // TODO: Fix styling, didn't work after implementing map function
 class ContactInfo extends Component {
     render() {
-
+        return(<p>Hello</p>);
     }
 }
 
