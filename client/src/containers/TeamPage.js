9fbfd2d54e4343251331cc1e81d18d87d571267c
kristandre <kra1992@hotmail.com>
2017-11-09 16:26:56 +0100

    Simplify TeamPage component

--- client/src/containers/TeamPage.js
+++ client/src/containers/TeamPage.js
@@ -1,47 +1,31 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import { fetchDepartments } from '../actions/department';
+import {connect} from 'react-redux';
 
 import TeamOverview from '../components/TeamOverview/TeamOverview';
 import PageHeader from '../components/PageHeader';
 import './TeamPage.css';
 
-class TeamPage extends Component {
-  componentDidMount() {
-    if (!this.props.departments.length > 0) {
-      this.props.fetchDepartments();
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="container">
-          <PageHeader>
-            <h1>Team</h1>
-            <p>Teamene har ansvar for alt fra rekruttering til drift av
-              nettsiden, sponsorer og lignende. Alle organisasjonelle oppgaver tas hånd om av
-              frivillige teammedlemmer. <span className="medium-bold">Vi får vektorprogrammet til å gå rundt! </span>
-            </p>
-          </PageHeader>
-          {/*<GradientBox/>*/}
-        </div>
-        <div className="team-page-overview">
-          <TeamOverview departments={this.props.departments}/>
-        </div>
-      </div>
-    );
-  }
-}
+const TeamPage = ({departments}) => (
+  <div>
+    <div className="container">
+      <PageHeader>
+        <h1>Team</h1>
+        <p>Teamene har ansvar for alt fra rekruttering til drift av
+          nettsiden, sponsorer og lignende. Alle organisasjonelle oppgaver tas hånd om av
+          frivillige teammedlemmer. <span
+            className="medium-bold">Vi får vektorprogrammet til å gå rundt! </span>
+        </p>
+      </PageHeader>
+    </div>
+    <div className="team-page-overview">
+      <TeamOverview departments={departments}/>
+    </div>
+  </div>
+);
 
 const mapStateToProps = state => ({
-  departments: state.departments,
+  departments: state.departments
 });
 
-const mapDispatchToProps = dispatch => bindActionCreators({
-  fetchDepartments,
-}, dispatch);
-
-export default connect(mapStateToProps, mapDispatchToProps)(TeamPage);
+export default connect(mapStateToProps)(TeamPage);
