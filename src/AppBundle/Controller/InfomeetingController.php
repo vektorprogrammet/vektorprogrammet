176f7f7f60bdf988f284986c45a2444c1deb2776
jvals <jvals@users.noreply.github.com>
2018-01-16 18:58:43 +0100

    One info meeting route for every department

--- src/AppBundle/Controller/InfomeetingController.php
+++ src/AppBundle/Controller/InfomeetingController.php
@@ -2,12 +2,14 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Department;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class InfomeetingController extends Controller
 {
-    public function showAction()
+    public function showAction(Department $department)
     {
-        return $this->render('info_meeting/info_meeting_admin.html.twig');
+        return $this->render('info_meeting/info_meeting_admin.html.twig',
+            array('department' => $department));
     }
 }
