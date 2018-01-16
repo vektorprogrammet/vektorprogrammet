9239322e16c67923573c5e24a0f32026c3150497
jvals <jvals@users.noreply.github.com>
2018-01-16 17:28:17 +0100

    Add infomeeting entity

---
+++ src/AppBundle/Entity/InfoMeeting.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace AppBundle\Entity;
+
+
+class InfoMeeting
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=250)
+     * @Assert\NotBlank
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="string", length=250)
+     * @Assert\NotBlank
+     */
+    private $time;
+
+    /**
+     * @ORM\Column(type="string", length=250)
+     * @Assert\NotBlank
+     */
+    private $room;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param mixed $date
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTime()
+    {
+        return $this->time;
+    }
+
+    /**
+     * @param mixed $time
+     */
+    public function setTime($time)
+    {
+        $this->time = $time;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRoom()
+    {
+        return $this->room;
+    }
+
+    /**
+     * @param mixed $room
+     */
+    public function setRoom($room)
+    {
+        $this->room = $room;
+    }
+
+
+
+
+}
