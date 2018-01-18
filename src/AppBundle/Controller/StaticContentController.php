7e5708f67787d72814f5cfeb7619888602461b2f
kristandre <kra1992@hotmail.com>
2018-01-18 16:57:18 +0100

    Style about page

--- src/AppBundle/Controller/StaticContentController.php
+++ src/AppBundle/Controller/StaticContentController.php
@@ -2,24 +2,36 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\StaticContent;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class StaticContentController extends Controller
 {
     /**
      * Updates the static text content in database.
      *
+     * @param Request $request
      * @return JsonResponse
      */
-    public function updateAction()
+    public function updateAction(Request $request)
     {
-        $req = $this->get('request');
-        $htmlId = $req->get('editorID');
-        $newContent = $req->get('editabledata');
-        $em = $this->getDoctrine()->getEntityManager();
-        $content = $em->getRepository('AppBundle:StaticContent')
-            ->findOneByHtmlId($htmlId);
+        $htmlId = $request->get('editorID');
+        dump($htmlId);
+        $newContent = $request->get('editabledata', '');
+        if (!$htmlId) {
+            throw new BadRequestHttpException("Invalid htmlID $htmlId");
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $content = $em->getRepository('AppBundle:StaticContent')->findOneByHtmlId($htmlId);
+        if (!$content) {
+            $content = new StaticContent();
+            $content->setHtmlId($htmlId);
+        }
+
         $content->setHtml($newContent);
         $em->persist($content);
         $em->flush();
