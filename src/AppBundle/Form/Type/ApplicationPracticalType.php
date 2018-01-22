0717e238b926708fce89c85b38ee3cf7faeb1fe0
khejd <khejd@hotmail.com>
2018-01-22 19:31:29 +0100

    Changed from boolean 'english' to string 'language'

--- src/AppBundle/Form/Type/ApplicationPracticalType.php
+++ src/AppBundle/Form/Type/ApplicationPracticalType.php
@@ -48,11 +48,12 @@ class ApplicationPracticalType extends AbstractType
             'multiple' => false,
         ));
 
-        $builder->add('english', 'choice', array(
+        $builder->add('language', 'choice', array(
             'label' => 'Vil du undervise på norsk skole eller internasjonal skole?',
             'choices' => array(
-                0 => 'Norsk',
-                1 => 'Engelsk',
+                'Norsk' => 'Norsk',
+                'Engelsk' => 'Engelsk',
+                'Norsk og engelsk' => 'Norsk og engelsk',
             ),
             'expanded' => true,
             'multiple' => false,
