c5fbb5921b2ebe385d5b9b4bdf43f8afbb05489d
Vilde Barth <vilde_barth@hotmail.com>
2018-01-22 17:29:57 +0100

    Applied codestyle

--- src/AppBundle/Entity/Survey.php
+++ src/AppBundle/Entity/Survey.php
@@ -198,7 +198,6 @@ class Survey implements \JsonSerializable
                     'answerText' => $answer->getAnswer(),
                     'schoolName' => $answer->getSurveyTaken()->getSchool()->getName()
                 );
-
             }
         }
 
