b8efd91017b37693ac9a4dbb14cc90ebbd0614a6
jvals <jvals@users.noreply.github.com>
2018-01-22 10:09:36 +0100

    Add form for requesting a new interview

---
+++ src/AppBundle/Form/InterviewNewTimeType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Form;
+
+use AppBundle\Entity\Interview;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class InterviewNewTimeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('newTimeMessage', TextareaType::class, array (
+                'label' => 'Melding',
+            ))
+            ->add('send', SubmitType::class, array(
+                'label' => 'Bekreft',
+            ));
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Interview::class
+        ));
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'app_bundle_interview_new_time_type';
+    }
+}
