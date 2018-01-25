82bf876aa2822f0a83498fc86b466b9531ad0873
jvals <jvals@users.noreply.github.com>
2018-01-25 18:06:16 +0100

    New interview textfield (#725)
    * Intervju tid -> Intervjutid
    
    * Add form for requesting a new interview
    
    * Add twig for requesting a new interview
    
    * Add newTimeMessage to Interview
    
    * Add form to requestNewTimeAction
    
    * Extend textArea for newTimeMessage
    
    * Add newTimeMessage to mail
    
    * Codestyle
    
    * Update tests
    
    * Add db migration
    
    * Assert that newTimeMessage is not null
    
    * Update migration
    
    * Use NotBlank over NotNull
    
    * Use bold over italics
    
    * Increase max length to 2000
    
    * Add length to newTimeMessage
    
    * 404 if invitation has been answered
    
    * Add missing use statement

--- src/AppBundle/Entity/Interview.php
+++ src/AppBundle/Entity/Interview.php
@@ -100,6 +100,15 @@ class Interview
     private $cancelMessage;
 
     /**
+     * @ORM\Column(type="string", length=2000)
+     * @Assert\Length(max=2000)
+     * @Assert\NotBlank()
+     *
+     * @var string
+     */
+    private $newTimeMessage;
+
+    /**
      * Constructor.
      */
     public function __construct()
@@ -108,6 +117,7 @@ class Interview
         $this->conducted = new \DateTime();
         $this->interviewed = false;
         $this->interviewStatus = InterviewStatusType::NO_CONTACT;
+        $this->newTimeMessage = "";
     }
 
     /**
@@ -524,5 +534,21 @@ class Interview
         } else {
             throw new InvalidArgumentException('Invalid status');
         }
+    }
+
+    /**
+     * @return string
+     */
+    public function getNewTimeMessage(): string
+    {
+        return $this->newTimeMessage;
+    }
+
+    /**
+     * @param string $newTimeMessage
+     */
+    public function setNewTimeMessage(string $newTimeMessage)
+    {
+        $this->newTimeMessage = $newTimeMessage;
     }
 }
---
+++ src/AppBundle/Form/InterviewNewTimeType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AppBundle\Form;
+
+use AppBundle\Entity\Interview;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class InterviewNewTimeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('newTimeMessage', TextareaType::class, array(
+                'label' => 'Melding',
+                'attr' => array('rows' => '5'),
+            ))
+            ->add('send', SubmitType::class, array(
+                'label' => 'Bekreft',
+            ));
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Interview::class
+        ));
+    }
+
+    public function getName()
+    {
+        return 'InterviewNewTime';
+    }
+}
