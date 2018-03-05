d1f0e370da1e31f7bd29b3100d372527cd197035
kristandre <kra1992@hotmail.com>
2018-03-05 18:41:17 +0100

    Unuglify vendor.js

--- gulpfile.js
+++ gulpfile.js
@@ -128,12 +128,11 @@ gulp.task('vendor', function () {
 
   gulp.src([
     'node_modules/jquery/dist/jquery.min.js',
-    'node_modules/popperjs/dist/popper.min.js',
+    'node_modules/popper.js/dist/umd/popper.min.js',
     'node_modules/bootstrap/dist/js/bootstrap.min.js',
     'node_modules/moment/min/moment.min.js'
   ])
     .pipe(concat('vendor.js'))
-    .pipe(uglify())
     .pipe(gulp.dest(path.dist + 'js/'));
 });
 
