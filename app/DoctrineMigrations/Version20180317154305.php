08b316f6b3ae1dc28362f58ee04c6361b507a498
Kristoffer <kra1992@hotmail.com>
2018-03-17 16:49:55 +0100

    Update migration

--- app/DoctrineMigrations/Version20180317154305.php
+++ app/DoctrineMigrations/Version20180317154305.php
@@ -5,9 +5,6 @@ namespace Application\Migrations;
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 class Version20180317154305 extends AbstractMigration
 {
     /**
@@ -15,10 +12,10 @@ class Version20180317154305 extends AbstractMigration
      */
     public function up(Schema $schema)
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE receipt ADD refundDate DATETIME DEFAULT NULL');
+        $this->addSql('UPDATE receipt SET refundDate = receiptDate');
     }
 
     /**
@@ -26,7 +23,6 @@ class Version20180317154305 extends AbstractMigration
      */
     public function down(Schema $schema)
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE receipt DROP refundDate');
