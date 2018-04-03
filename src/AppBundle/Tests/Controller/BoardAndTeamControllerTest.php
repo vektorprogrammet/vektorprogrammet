d6bbbeda2e13188b6320586adda72d11df71df77
kristandre <kristoffer.r.andresen@gmail.com>
2018-04-03 18:15:59 +0200

    Fix Team page tests

--- src/AppBundle/Tests/Controller/BoardAndTeamControllerTest.php
+++ src/AppBundle/Tests/Controller/BoardAndTeamControllerTest.php
@@ -8,22 +8,12 @@ class BoardAndTeamControllerTest extends BaseWebTestCase
 {
     public function testShow()
     {
-        $client = static::createClient();
+        $crawler = $this->goTo('/team');
+	    $this->assertEquals(1, $crawler->filter('h1:contains("team")')->count());
 
-        $crawler = $client->request('GET', '/styretogteam');
-
-        // Assert that we have the correct page
-        $this->assertContains('Styret og team', $client->getResponse()->getContent());
-
-        // Assert that we have the correct team(s)
-        $this->assertContains('Styret', $client->getResponse()->getContent());
-        $this->assertContains('IT', $client->getResponse()->getContent());
-
-        // Check the count for the parameters
-        $this->assertEquals(1, $crawler->filter('html:contains("Styret")')->count());
-        $this->assertEquals(1, $crawler->filter('html:contains("IT")')->count());
-
-        // Assert a specific 200 status code
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+	    $teamOverview = $crawler->filter('.team-overview');
+	    $tabs = $teamOverview->filter('#team-overview-tabs');
+	    $this->assertEquals(5, $tabs->filter('a')->count());
+	    $this->assertGreaterThan(5, $teamOverview->filter('.team-card')->count());
     }
 }
