740d46d8647c454d34dcae128df21581ef391f5e
jvals <jvals@users.noreply.github.com>
2018-04-03 17:05:19 +0200

    Reset -> Tilbakestill

--- src/AppBundle/Tests/Controller/PasswordResetControllerTest.php
+++ src/AppBundle/Tests/Controller/PasswordResetControllerTest.php
@@ -13,7 +13,7 @@ class PasswordResetControllerTest extends BaseWebTestCase
         $crawler = $client->request('GET', '/resetpassord');
 
         // Assert that we have the correct amount of data
-        $this->assertEquals(1, $crawler->filter('h4:contains("Reset passord")')->count());
+        $this->assertEquals(1, $crawler->filter('h4:contains("Tilbakestill passord")')->count());
 
         // Assert a specific 200 status code
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
