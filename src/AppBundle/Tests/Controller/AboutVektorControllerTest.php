bbbbcdd6f000d17dc7fe0865c95e8ad4375e4375
kristandre <kristoffer.r.andresen@gmail.com>
2018-04-03 17:49:15 +0200

    Fix AboutVektor tests

--- src/AppBundle/Tests/Controller/AboutVektorControllerTest.php
+++ src/AppBundle/Tests/Controller/AboutVektorControllerTest.php
@@ -11,18 +11,6 @@ class AboutVektorControllerTest extends BaseWebTestCase
         $crawler = $this->goTo('/omvektor');
 
         $this->assertEquals(1, $crawler->filter('h1:contains("Om Vektorprogrammet")')->count());
-        $this->assertEquals(1, $crawler->filter('h1:contains("Mål og verdier")')->count());
-        $this->assertEquals(1, $crawler->filter('h1:contains("Historie")')->count());
-
-        $this->assertEquals(1, $crawler->filter('strong:contains("Vektorprogrammet arbeider for å øke interessen for matematikk og realfag blant elever i grunnskolen. ")')->count());
-        $this->assertEquals(1, $crawler->filter('li:contains("Vektorprogrammet ønsker å øke matematikkforståelsen blant elever i grunnskolen.")')->count());
-        $this->assertEquals(1, $crawler->filter('p:contains("Vektorprogrammet er et initiativ som ble startet av linjeforeningen Nabla i september 2010")')->count());
-    }
-
-    public function testShowFaq()
-    {
-        $crawler = $this->goTo('/faq');
-
-        $this->assertEquals(1, $crawler->filter('h1:contains("Ofte stilte spørsmål")')->count());
+        $this->assertEquals(1, $crawler->filter('h2:contains("Ofte stilte spørsmål og svar")')->count());
     }
 }
