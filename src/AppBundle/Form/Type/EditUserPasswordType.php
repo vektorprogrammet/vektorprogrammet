1197f661fa2d76995b54e0bac587de7501594b7a
kristandre <kristoffer.r.andresen@gmail.com>
2018-04-09 17:27:38 +0200

    Assert password max length 64 characters

--- src/AppBundle/Form/Type/EditUserPasswordType.php
+++ src/AppBundle/Form/Type/EditUserPasswordType.php
@@ -5,6 +5,7 @@ namespace AppBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class EditUserPasswordType extends AbstractType
 {
@@ -15,7 +16,14 @@ class EditUserPasswordType extends AbstractType
                 'first_name' => 'Passord',
                 'second_name' => 'Gjenta_passord',
                 'type' => 'password',
-            ))
+                'invalid_message' => 'Passordene må være like',
+                'constraints' => array(new Assert\Length(array(
+                    'min' => 8,
+                    'max' => 64,
+                    'minMessage' => 'Passordet må være på minst {{ limit }} tegn',
+                    'maxMessage' => 'Passordet må være mindre enn {{ limit }} tegn langt',
+                ))
+            )))
             ->add('save', 'submit', array(
                 'label' => 'Lagre',
             ));
