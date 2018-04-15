4eb42eff1eea7f613a110bfdd55d40175938d49d
jvals <jvals@users.noreply.github.com>
2018-04-15 20:58:59 +0200

    404 if team is not active and user is not team member or higher

--- src/AppBundle/Controller/TeamController.php
+++ src/AppBundle/Controller/TeamController.php
@@ -3,12 +3,16 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class TeamController extends Controller
 {
     public function showAction($id)
     {
         $team = $this->getDoctrine()->getRepository('AppBundle:Team')->find($id);
+        if (!$team->getisActive() and !$this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')) {
+            throw new NotFoundHttpException('Team not found');
+        }
         $workHistories = $this->getDoctrine()->getRepository('AppBundle:WorkHistory')->findActiveWorkHistoriesByTeam($team);
 
         return $this->render('team/team_page.html.twig', array(
