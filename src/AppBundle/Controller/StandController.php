48a5636721355095cd14810272a76c2a0ecd2b9e
Kristoffer <kra1992@hotmail.com>
2018-04-21 11:45:43 +0200

    Add link to interest list in control panel

---
+++ src/AppBundle/Controller/StandController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Semester;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class StandController extends Controller
+{
+	/**
+	 * @Route("/kontrollpanel/stand/{id}", name="stand_by_semester")
+	 * @Route("/kontrollpanel/stand", name="stand")
+	 *
+	 * @param Semester|null $semester
+	 *
+	 * @return \Symfony\Component\HttpFoundation\Response
+	 */
+    public function indexAction(Semester $semester = null)
+    {
+    	if ($semester === null) {
+    		$semester = $this->getUser()->getDepartment()->getCurrentOrLatestSemester();
+	    }
+        return $this->render('stand_admin/stand.html.twig', [
+        	'semester' => $semester
+        ]);
+    }
+}
