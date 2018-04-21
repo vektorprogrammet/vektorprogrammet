771b0c50cc3d6572a514514254c335c79b9c3fdc
vektorbot <vektorbot@vektorprogrammet.no>
2018-04-21 11:46:24 +0200

    Fix code style

--- src/AppBundle/Controller/StandController.php
+++ src/AppBundle/Controller/StandController.php
@@ -8,21 +8,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class StandController extends Controller
 {
-	/**
-	 * @Route("/kontrollpanel/stand/{id}", name="stand_by_semester")
-	 * @Route("/kontrollpanel/stand", name="stand")
-	 *
-	 * @param Semester|null $semester
-	 *
-	 * @return \Symfony\Component\HttpFoundation\Response
-	 */
+    /**
+     * @Route("/kontrollpanel/stand/{id}", name="stand_by_semester")
+     * @Route("/kontrollpanel/stand", name="stand")
+     *
+     * @param Semester|null $semester
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function indexAction(Semester $semester = null)
     {
-    	if ($semester === null) {
-    		$semester = $this->getUser()->getDepartment()->getCurrentOrLatestSemester();
-	    }
+        if ($semester === null) {
+            $semester = $this->getUser()->getDepartment()->getCurrentOrLatestSemester();
+        }
         return $this->render('stand_admin/stand.html.twig', [
-        	'semester' => $semester
+            'semester' => $semester
         ]);
     }
 }
