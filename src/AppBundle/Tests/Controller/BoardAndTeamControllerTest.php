39736251b6524cb19c0ab27354d29f3cbc65089c
jvals <jvals@users.noreply.github.com>
2018-04-24 20:51:50 +0200

    Add test for hiding teams

--- src/AppBundle/Tests/Controller/BoardAndTeamControllerTest.php
+++ src/AppBundle/Tests/Controller/BoardAndTeamControllerTest.php
@@ -16,4 +16,21 @@ class BoardAndTeamControllerTest extends BaseWebTestCase
         $this->assertEquals(5, $tabs->filter('a')->count());
         $this->assertGreaterThan(5, $teamOverview->filter('.team-card')->count());
     }
+
+    public function testHideTeam()
+    {
+        $crawler = $this->goTo('/team#NTNU');
+        $numberOfTeamsBefore = $crawler->filter('.team-card')->count();
+
+        $client = self::createAdminClient();
+        $crawler = $this->goTo('/kontrollpanel/teamadmin/update/2', $client);
+        $form = $crawler->selectButton('Lagre')->form();
+        $form['createTeam[isActive]']->untick();
+        $client->submit($form);
+
+        $crawler = $this->goTo('/team#NTNU');
+        $numberOfTeamsAfter = $crawler->filter('.team-card')->count();
+
+        $this->assertEquals($numberOfTeamsBefore-1, $numberOfTeamsAfter);
+    }
 }
