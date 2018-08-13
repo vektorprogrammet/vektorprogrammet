2248895a8769e9636d7d2744c36589981959f418
vektorbot <vektorbot@vektorprogrammet.no>
2018-08-13 19:12:19 +0200

    Create database migration

---
+++ app/DoctrineMigrations/Version20180813171219.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180813171219 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE department CHANGE city city VARCHAR(250) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_CD1DE18A2D5B0234 ON department (city)');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP INDEX UNIQ_CD1DE18A2D5B0234 ON department');
+        $this->addSql('ALTER TABLE department CHANGE city city VARCHAR(250) DEFAULT NULL');
+    }
+}
