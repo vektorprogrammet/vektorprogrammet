0e2a267940af0087bd826fe979cc2eaff8df499e
Sigurd Totland <sigurdtotland@gmail.com>
2018-08-13 12:33:45 +0200

    Show call to action link on assistant page (#910)
    * Show call to action link on assistant page
    
    * Fix unwanted text breaking on small phones
    
    * Only slim margins on element right below jumbotron
    
    * Filter departments in Service\FilterService instead of in twig
    
    * Prettify CTA and increase bottom margin
    
    * Don't use thin header and decrease section margin to match it
    
    * Fix button text and margin
    
    * Change text
    
    * Move CTA further up

--- src/AppBundle/Service/FilterService.php
+++ src/AppBundle/Service/FilterService.php
@@ -26,4 +26,25 @@ class FilterService
         }
         return $filtered;
     }
+
+    /**
+     * Returns only departments with active admission set to $hasActiveAdmission
+     *
+     * @param \AppBundle\Entity\Department[] $departments
+     * @param boolean $hasActiveAdmission
+     *
+     * @return \AppBundle\Entity\Department[]
+     */
+    public function filterDepartmentsByActiveAdmission($departments, $hasActiveAdmission)
+    {
+        $filtered = [];
+        foreach ($departments as $department) {
+            $currentSemester = $department->getCurrentSemester();
+            $departmentHasActiveAdmission = ($currentSemester !== null && $currentSemester->hasActiveAdmission());
+            if ($departmentHasActiveAdmission === $hasActiveAdmission) {
+                $filtered[] = $department;
+            }
+        }
+        return $filtered;
+    }
 }
