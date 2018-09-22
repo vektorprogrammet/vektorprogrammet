f061e4571a0867d6b8ce8037a79e288c600e32a6
Kristoffer <kra1992@hotmail.com>
2018-09-22 14:15:04 +0200

    Remove unused component

--- client/src/views/MyPageView.vue
+++ client/src/views/MyPageView.vue
@@ -1,13 +1,16 @@
 <template>
-  
+  <PageHeader>
+    <h1>Min side</h1>
+  </PageHeader>
 </template>
 
 <script>
+  import PageHeader from '../components/PageHeader';
   export default {
     name: 'MyPageView',
+    components: {PageHeader},
   };
 </script>
 
 <style scoped>
-
 </style>
