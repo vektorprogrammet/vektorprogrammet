6a02af43959f1b99513d8ada2c097e0160889de3
Amir Ahmed <Amir.ahmed.1998@gmail.com>
2018-11-19 19:36:34 +0100

    migration

--- app/DoctrineMigrations/Version20181119182727.php
+++ app/DoctrineMigrations/Version20181119182727.php
@@ -15,11 +15,12 @@ final class Version20181119182727 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE user ADD reservedFromPopUp TINYINT(1) NOT NULL, ADD lastPopUpTime DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE user ADD reservedFromPopUp TINYINT(1) NOT NULL DEFAULT \'0\',
+                                            ADD lastPopUpTime DATETIME NOT NULL DEFAULT GETDATE()');
         $this->addSql('ALTER TABLE survey_taken ADD user_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE survey_taken ADD CONSTRAINT FK_B3982430A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE SET NULL');
         $this->addSql('CREATE INDEX IDX_B3982430A76ED395 ON survey_taken (user_id)');
-        $this->addSql('ALTER TABLE survey ADD showCustomPopUpMessage TINYINT(1) NOT NULL, ADD teamSurvey TINYINT(1) DEFAULT \'0\' NOT NULL, ADD surveyPopUpMessage LONGTEXT NOT NULL, CHANGE showCustomFinishPage showCustomFinishPage TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE survey ADD showCustomPopUpMessage TINYINT(1) NOT NULL, ADD teamSurvey TINYINT(1) DEFAULT \'0\' NOT NULL, ADD surveyPopUpMessage LONGTEXT NOT NULL DEFAULT \'\', CHANGE showCustomFinishPage showCustomFinishPage TINYINT(1) NOT NULL DEFAULT \'0\'');
     }
 
     public function down(Schema $schema) : void
