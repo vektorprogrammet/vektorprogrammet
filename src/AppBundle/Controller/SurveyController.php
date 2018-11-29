1991e37c635dad9e685134a220adcd6f495484fa
Amir Ahmed <Amir.ahmed.1998@gmail.com>
2018-11-29 20:21:01 +0100

    test paramteres reverted

--- src/AppBundle/Controller/SurveyController.php
+++ src/AppBundle/Controller/SurveyController.php
@@ -359,12 +359,6 @@ class SurveyController extends BaseController
     {
         $this->get(SurveyManager::class)->toggleReservedFromPopUp($this->getUser());
 
-        $user = $this->getUser();
-        $user->setLastPopUpTime(new \DateTime("30-12-2011"));
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($user);
-        $em->flush();
-
         return new JsonResponse();
     }
 
