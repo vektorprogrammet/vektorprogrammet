353a9ec2f851e845c29b0ac3e524f1e2ba4af1ff
kristandre <kra1992@hotmail.com>
2019-01-22 19:34:48 +0100

    Create receipt widget

--- src/AppBundle/Controller/WidgetController.php
+++ src/AppBundle/Controller/WidgetController.php
@@ -2,6 +2,9 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Receipt;
+use AppBundle\Utils\ReceiptStatistics;
+
 class WidgetController extends BaseController
 {
     public function interviewsAction()
@@ -18,5 +21,19 @@ class WidgetController extends BaseController
         }
 
         return $this->render('widgets/interviews_widget.html.twig', ['applications' => $applicationsAssignedToUser]);
+    }
+
+    public function receiptsAction()
+    {
+        $usersWithReceipts = $this->getDoctrine()->getRepository('AppBundle:User')->findAllUsersWithReceipts();
+        $pendingReceipts = $this->getDoctrine()->getRepository('AppBundle:Receipt')->findByStatus(Receipt::STATUS_PENDING);
+
+        $pendingReceiptStatistics = new ReceiptStatistics($pendingReceipts);
+
+        return $this->render('widgets/receipts_widget.html.twig', [
+            'users_with_receipts' => $usersWithReceipts,
+            'receitps' => $pendingReceipts,
+            'statistics' => $pendingReceiptStatistics
+        ]);
     }
 }
