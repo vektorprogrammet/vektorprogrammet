9812494754053b113099a6c36648b885417b0c13
Mathias Wahl <ma10asw@gmail.com>
2019-01-22 20:30:21 +0100

    introduces vue file with counter

---
+++ client/src/views/partypage/PartyPageView.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <div id="Title">
+            <h1>Vektor Party</h1>
+        </div>
+
+        <div id="animated-number-demo">
+            <input v-model.number="number" type="number" step="20">
+            <p>{{ animatedNumber }}</p>
+        </div>
+
+        <div id="number-of-applicants">
+            <p>{{ number_of_applicants }}</p>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    import TweenLite from 'gsap'
+    import axios from 'axios'
+    export default {
+        name: "PartyPageView",
+        data() {
+            return {
+                number: 0,
+                tweenedNumber: 0,
+                number_of_applicants: 0
+            }
+        },
+        computed: {
+            animatedNumber: function() {
+                return this.tweenedNumber.toFixed(0);
+            },
+        },
+        watch: {
+            number: function(newValue) {
+                TweenLite.to(this.$data, 10, { tweenedNumber: newValue });
+            }
+        },
+        mounted () {
+            axios
+                .get('localhost:8000/')
+                .then(this.tweenedNumber = 100)
+                .then(response => ( dataa = response ))
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
