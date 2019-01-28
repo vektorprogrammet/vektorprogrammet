6c95d0a14604ba3251fe652ab65619a7fab2746c
sigtot <sigurdtotland@gmail.com>
2019-01-28 21:39:57 +0100

    Add route to party page in ClientController

--- src/AppBundle/Controller/ClientController.php
+++ src/AppBundle/Controller/ClientController.php
@@ -10,6 +10,7 @@ class ClientController extends BaseController
     /**
      * @Route("/kontrollpanel/staging", name="staging_servers_show")
      * @Route("/assistent/min-side")
+     * @Route("/party")
      *
      * @return \Symfony\Component\HttpFoundation\Response
      */
