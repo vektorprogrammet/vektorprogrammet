1797c2bc5280aa66a999321139dc5b4b840bd0ab
amir <amir.ahmed.1998@gmail.com>
2019-01-31 23:22:07 +0100

    showcase different, somewhat broken, designs

--- src/AppBundle/Controller/ControlPanelController.php
+++ src/AppBundle/Controller/ControlPanelController.php
@@ -10,23 +10,13 @@ class ControlPanelController extends BaseController
     {
         $department = $this->getDepartmentOrThrow404();
         $semester = $this->getSemesterOrThrow404();
-        $user = $this->getUser();
 
         $admissionPeriod = $this->getDoctrine()->getRepository('AppBundle:AdmissionPeriod')
             ->findOneByDepartmentAndSemester($department, $semester);
 
-
-
-        $surveys = $this->getDoctrine()
-            ->getRepository('AppBundle:Survey')
-            ->findAllNotTakenByUserAndSemester($user, $semester);
-        $isAvailableSurveys = !empty($surveys);
-
-
         // Return the view to be rendered
         return $this->render('control_panel/index.html.twig', array(
             'admissionPeriod' => $admissionPeriod,
-            'isAvailableSurveys' => $isAvailableSurveys,
         ));
     }
 
