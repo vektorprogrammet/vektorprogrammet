2b9b724ff0de9f2e227d01019228235d385a3afc
Mathias Wahl <ma10asw@gmail.com>
2019-03-13 12:50:32 +0100

    add directions service to map on assistand dashboard

--- client/src/components/Map.vue
+++ client/src/components/Map.vue
@@ -1,45 +1,15 @@
 <template>
-      <!--GmapMap
-              :center="{lat:home_location.lat, lng:home_location.lng}"
-              :zoom="10"
-              map-type-id="roadmap"
-              style="width: 100%; height: 500px"
-      >
-            <GmapMarker
-                    key=1
-                    :position= "school_location"
-                    :clickable="true"
-                    :draggable="true"
-                    @click="center=position"
-            />
-            <GmapMarker
-                    key=2
-                    :position= "home_location"
-                    :clickable="true"
-                    :draggable="true"
-                    @click="center=position"
-            />
-      </GmapMap-->
-      <div id="map" style="width: 100%; height: 500px;">
-
+      <div class="content">
+            <div id="map" style="width: 100%; height: 500px;"></div>
+            <p>Reisen fra {{this.home_name}} til {{this.school_name}} ungdomsskole tar {{this.duration}} </p>
       </div>
-
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
-import {accountService} from "../services";
 import {mapService} from "../services";
 import { Prop } from 'vue-property-decorator';
-import * as VueGoogleMaps from 'vue2-google-maps';
-
-/*Vue.use(VueGoogleMaps, {
-    load: {
-        key: 'AIzaSyCfhjte_te7uOqtXmZkvtrhdZaNMaVIGso',
-        libraries: 'places',
-    },
-});*/
 
 @Component()
 
@@ -47,30 +17,69 @@ export default class Map extends Vue {
   @Prop() private school_name: string;
   @Prop() private home_name: string;
   @Prop() private school_location: JSON;
-  private home_location: any = {lat:63, lng:10};
-  private route: JSON;
+  private duration: string = ' X min';
 
   public async mounted() {
-      /*this.school_location = await mapService.getLocationByName(this.school_name + ' ungdomsskole');
-      this.school_location=this.school_location.results[0].geometry.location;
-      this.home_location = await mapService.getLocationByName(this.home_name);
-      this.home_location=this.home_location.results[0].geometry.location;
-      this.route = await accountService.getRouteBetweenAddresses(this.home_name, this.school_name + ' ungdomsskole');*/
       try {
           const google = await mapService.init();
           const geocoder = new google.maps.Geocoder();
-          //const map = new google.maps.Map(document.getElementById('Map'));
-          console.log(document.getElementById('map'));
+          const directionsService = new google.maps.DirectionsService();
+          const directionsDisplay = new google.maps.DirectionsRenderer();
           let map = new google.maps.Map(document.getElementById('map'));
-          console.log(map);
-          geocoder.geocode({ address: 'NTNU' }, (results, status) => {
+          directionsDisplay.setMap(map);
+
+
+          /*geocoder.geocode({ address: this.school_name + ' ungdomsskole'}, (results, status) => {
+              if (status !== 'OK' || !results[0]) {
+                  throw new Error(status);
+              }
+              map.setCenter(results[0].geometry.location);
+              map.setZoom(10);
+
+              this.locations.push({
+                  position:
+                      {lat: results[0].geometry.location.lat(), lng: results[0].geometry.location.lng()}
+              });
+              let marker = new google.maps.Marker({
+                  position: {lat: results[0].geometry.location.lat(), lng: results[0].geometry.location.lng()},
+                  map: map,
+                  title: this.school_name + ' ungdomsskole',
+              });
+          });
+
+          geocoder.geocode({ address: this.home_name}, (results, status) => {
               if (status !== 'OK' || !results[0]) {
                   throw new Error(status);
               }
 
-              map.setCenter(results[0].geometry.location);
-              map.fitBounds(results[0].geometry.viewport);
+              this.locations.push({
+                  position:
+                      {lat: results[0].geometry.location.lat(), lng: results[0].geometry.location.lng()}
+              });
+              let marker = new google.maps.Marker({
+                  position: {lat: results[0].geometry.location.lat(), lng: results[0].geometry.location.lng()},
+                  map: map,
+                  title: this.home_name,
+              });
           });
+
+          /*let markers = this.locations
+              .map(x => new google.maps.Marker({ ...x, map }));*/
+
+          let request = {
+              origin: this.home_name,
+              destination: this.school_name + ' ungdomsskole',
+              travelMode: 'TRANSIT'
+          };
+          directionsService.route(request, (result, status) => {
+              if (status !== 'OK') {
+                  throw new Error(status);
+              } else {
+                  directionsDisplay.setDirections(result);
+                  this.duration = result.routes[0].legs[0].duration.text
+              }
+          });
+
       } catch (error) {
           console.error(error);
       }
