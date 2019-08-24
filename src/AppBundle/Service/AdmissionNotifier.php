cab245f801de6fed687ed641bb0dd041398f3c88
Sigurd Totland <sigurdtotland@gmail.com>
2019-08-24 15:09:47 +0200

    Bare send varsel om infomøte hvis det er et kommende infomøte samme dag. + tester (#1202)
    * Only send info meeting notifications on same day as info meeting.
    
    Also refactor this logic into utils InfoMeetingUtil and TimeUtil
    
    * Write unit tests for InfoMeetingUtil and TimeUtil
    
    * Move shouldSendInfoMeetingNotification into AdmissionPeriod class
    
    * Remove rogue InfoMeetingUtil import
    
    * Add check for info meeting being in the future before sending

--- src/AppBundle/Service/AdmissionNotifier.php
+++ src/AppBundle/Service/AdmissionNotifier.php
@@ -123,14 +123,8 @@ class AdmissionNotifier
         try {
             foreach ($departments as $department) {
                 $admissionPeriod = $this->em->getRepository('AppBundle:AdmissionPeriod')->findOneByDepartmentAndSemester($department, $semester);
-                if ($admissionPeriod === null || $admissionPeriod->getInfoMeeting() === null || !$admissionPeriod->getInfoMeeting()->isShowOnPage()) {
-                    continue;
-                }
 
-                $now = new \DateTime();
-                $infoMeetingLessThanOneDay = $admissionPeriod->getInfoMeeting()->getDate()->diff($now)->d <= 1;
-                $lessThanOneDayLeft = $infoMeetingLessThanOneDay && $admissionPeriod->getInfoMeeting()->getDate() > $now;
-                if (!$lessThanOneDayLeft) {
+                if ($admissionPeriod === null || !$admissionPeriod->shouldSendInfoMeetingNotifications()) {
                     continue;
                 }
 
---
+++ tests/AppBundle/Util/TimeUtilTest.php
@@ -0,0 +1,43 @@
+<?php
+
+use AppBundle\Utils\TimeUtil;
+use PHPUnit\Framework\TestCase;
+
+class TimeUtilTest extends TestCase
+{
+    public function testDateTimeIsTodayWithTodayDate()
+    {
+        $today = new \DateTime();
+        $this->assertTrue(TimeUtil::dateTimeIsToday($today));
+    }
+
+    public function testDateTimeIsTodayWithYesterdaysDate()
+    {
+        $yesterday = (new \DateTime())->modify('-1 day');
+        $this->assertFalse(TimeUtil::dateTimeIsToday($yesterday));
+    }
+
+    public function testDateTimeIsTodayWithTomorrowsDate()
+    {
+        $tomorrow = (new \DateTime())->modify('+1 day');
+        $this->assertFalse(TimeUtil::dateTimeIsToday($tomorrow));
+    }
+
+    public function testDateTimeIsInTheFutureWithFutureDate()
+    {
+        $tomorrow = (new \DateTime())->modify('+1 day');
+        $this->assertTrue(TimeUtil::dateTimeIsInTheFuture($tomorrow));
+    }
+
+    public function testDateTimeIsInTheFutureWithPastDate()
+    {
+        $yesterday = (new \DateTime())->modify('-1 day');
+        $this->assertFalse(TimeUtil::dateTimeIsInTheFuture($yesterday));
+    }
+
+    public function testDateTimeIsInTheFutureWithCurrentDateTime()
+    {
+        $now = new \DateTime();
+        $this->assertFalse(TimeUtil::dateTimeIsInTheFuture($now));
+    }
+}
