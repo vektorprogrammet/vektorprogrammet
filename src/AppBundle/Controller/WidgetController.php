b423ab89578c8a15a6592b466a1d837f62ef9869
sigtot <sigurdtotland@gmail.com>
2019-08-30 19:48:44 +0200

    Show wholesome message in receipt widget when all receipts are paid

--- src/AppBundle/Controller/WidgetController.php
+++ src/AppBundle/Controller/WidgetController.php
@@ -34,13 +34,14 @@ class WidgetController extends BaseController
         $sorter->sortUsersByReceiptStatus($usersWithReceipts);
 
         $pendingReceipts = $this->getDoctrine()->getRepository('AppBundle:Receipt')->findByStatus(Receipt::STATUS_PENDING);
-
         $pendingReceiptStatistics = new ReceiptStatistics($pendingReceipts);
+
+        $noReceipts = empty($usersWithReceipts);
 
         return $this->render('widgets/receipts_widget.html.twig', [
             'users_with_receipts' => $usersWithReceipts,
-            'receitps' => $pendingReceipts,
-            'statistics' => $pendingReceiptStatistics
+            'statistics' => $pendingReceiptStatistics,
+            'no_receipts' => $noReceipts,
         ]);
     }
 
