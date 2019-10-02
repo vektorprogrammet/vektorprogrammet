c80bccc25ef28e0866f5aa8e8ca2f423b66f0a2c
lundli <lundli@pm.me>
2019-10-02 18:12:08 +0200

    typo fix
    Signed-off-by: lundli <lundli@pm.me>

--- src/AppBundle/Form/Type/ReceiptType.php
+++ src/AppBundle/Form/Type/ReceiptType.php
@@ -38,7 +38,7 @@ class ReceiptType extends AbstractType
                 'label' => false,
             ))
             ->add('picturePath', FileType::class, array(
-                'label' => 'Velg bilde av kvitteringen: ',
+                'label' => 'Velg/endre bilde av kvitteringen: ',
                 'required' => $options['picture_required'],
                 'data_class' => null,
                 'attr' => array('class' => 'receipt-upload-hack', 'accept' => 'image/*'),
