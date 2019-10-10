7dcbb408075970c24efca07584d26c873ff15b2a
lundli <lundli@pm.me>
2019-10-10 10:26:56 +0200

    started writing test for creating an event
    Signed-off-by: lundli <lundli@pm.me>

--- tests/AppBundle/Controller/SocialEventControllerTest.php
+++ tests/AppBundle/Controller/SocialEventControllerTest.php
@@ -41,5 +41,30 @@ class SocialEventControllerTest extends BaseWebTestCase
     }
 
 
+    public function testCreate()
+    {
+
+
+        // UNCOMPLETE FUNCTION //
+        $client = $this->createAdminClient();
+        $crawler = $client->request('GET', '/kontrollpanel/changelog/create');
+        $form = $crawler->selectButton('Lagre')->form();
+        $date = (new \DateTime())->modify('+1day')->format('d.m.Y H:m');
+        $form['change_log[title]'] = 'En ny endring';
+        $form['change_log[description]'] = 'Beskrivelse av endringen, slik at du forstår hva som er gjort';
+        $form['change_log[gitHubLink]'] = 'https://github.com/vektorprogrammet/vektorprogrammet';
+        $form['change_log[date]'] = $date;
+        $client->submit($form);
+        $crawler = $client->followRedirect();
+        $changeLogItemAfter = $crawler->filter('td:contains("En ny endring")')->count();
+        $this->assertEquals(1, $changeLogItemAfter);
+
+        // --------------- //
+        $client = $this->createAdminClient();
+        $crawler = $client->request('GET', '/kontrollpanel/arrangementer/opprett');
+
+
+    }
+
 
 }
