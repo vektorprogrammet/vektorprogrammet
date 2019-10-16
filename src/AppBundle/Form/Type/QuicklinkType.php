0c3100485ef7fa02a58f433ffb570207d503d50e
victorah96 <victoria.ahmadi@vektorprogrammet.no>
2019-10-16 18:09:45 +0200

    Revert "Revert "Jobbet mye med kontroller, men mangler tester""
    This reverts commit 3de4488269acf90da5c2622ba142968cb9d8d14b.

--- src/AppBundle/Form/Type/QuicklinkType.php
+++ src/AppBundle/Form/Type/QuicklinkType.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
@@ -12,13 +13,29 @@ class QuicklinkType extends AbstractType
     {
         $builder
             ->add("title", TextType::class, array(
-                'label' => "Skriv in titellen til QickLinken"
+                'label' => "Skriv in titellen til QickLinken",
+                "attr" => array(
+                    'placeholder' => "Google Drive"
+                )
 
             ))
-            ->add("url", TextType::class, array(
-                'label' => "Skriv in titellen til QickLinken"
 
+            ->add("url", TextType::class, array(
+                'label' => "Url'en til Quicklinken",
+                 "attr" => array(
+                    'placeholder'=> "www.drive.google.com"
+                )
+
+            ))
+
+            ->add('iconUrl', FileType::class, array(
+                'required' =>  true,
+                'error_bubbling' => true,
+                'data_class' => null,
+                'label' => 'Last opp ny logo',
             ));
+
+
     }
 }
 
