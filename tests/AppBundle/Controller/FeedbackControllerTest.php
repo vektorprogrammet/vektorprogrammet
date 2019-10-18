71d873d98b1cb972f5aff8c8c1b6a8b7ef731756
Hans Tjøtta <hahotjo@gmail.com>
2019-10-18 18:50:31 +0200

    Added error_feedback test

--- tests/AppBundle/Controller/FeedbackControllerTest.php
+++ tests/AppBundle/Controller/FeedbackControllerTest.php
@@ -57,7 +57,45 @@ class FeedbackControllerTest extends BaseWebTestCase
         //Checks if Title and Description are in the new Feedback
         $this->assertContains($testTitle, $crawler->filter('.card-header')->text());
         $this->assertContains($testDescription, $crawler->filter('#description')->text());
+    }
 
+    public function testErrorFeedback()
+    {
+        //Sets test-variables
+        $testTitle = 'Test-error-tittel';
+        $testDescription = 'Test-beskrivelse_error';
+        $testType = 'question';
 
+        $client = $this->createTeamMemberClient();
+        //count current feedbacks
+        $client->request('GET', '/kontrollpanel/feedback/list');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $noOfFeedbacksBefore = $this->countTableRows('/kontrollpanel/feedback/list', $client);
+
+        $anonClient = $this->createAnonymousClient();
+        //Submit new feedback
+        $crawler = $anonClient->request('GET', '/_error/500');
+        $this->assertEquals(200, $anonClient->getResponse()->getStatusCode());
+
+        //Submits form
+        $form = $crawler->selectButton('Send inn')->form([
+            'error_feedback[title]' => $testTitle,
+            'error_feedback[description]' => $testDescription
+        ]);
+        $anonClient->submit($form);
+
+        //check if new feedback was added
+        $noOfFeedbacksAfter = $this->countTableRows('/kontrollpanel/feedback/list', $client);
+        $this->assertEquals(1, $noOfFeedbacksAfter - $noOfFeedbacksBefore);
+
+        //check specific feedback
+        $crawler = $client->request('GET', '/kontrollpanel/feedback/list');
+        $link = $crawler->filter('tbody a')->first()->link();
+        $crawler = $client->request('GET', $link->getUri());
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        //Checks if Title and Description are in the new Feedback
+        $this->assertContains($testTitle, $crawler->filter('.card-header')->text());
+        $this->assertContains($testDescription, $crawler->filter('#description')->text());
     }
 }
