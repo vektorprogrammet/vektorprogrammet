c79c89fead9007783d84227e4fd272422cedb9b8
vektorbot <vektorbot@vektorprogrammet.no>
2019-10-22 19:58:32 +0200

    Create database migration

---
+++ app/DoctrineMigrations/Version20191022175832.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191022175832 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE change_log_item');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE change_log_item (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(40) NOT NULL COLLATE utf8mb4_unicode_ci, description VARCHAR(1000) DEFAULT NULL COLLATE utf8mb4_unicode_ci, githubLink VARCHAR(1000) NOT NULL COLLATE utf8mb4_unicode_ci, date DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB COMMENT = \'\' ');
+    }
+}
