a3d3afade5c4cdfc2c3dd74597629488c7104cc8
AJO <alex.andunge@gmail.com>
2019-11-04 16:36:14 +0100

    Made form for making parent-courses (ParentCourseType)

---
+++ src/AppBundle/Form/Type/ParentCourseType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace AppBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+
+
+class ParentCourseType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('speaker', TextType::class, array(
+                'label' => 'Skriv fullt navn på foredragsholder',
+                'required' => true,
+                'attr' => array(
+                    'autocomplete' => 'off',
+                ),
+            ))
+            ->add('place', TextType::class, array(
+                'label' => 'Skriv sted for kurset',
+                'required' => true,
+                'attr' => array(
+                    'autocomplete' => 'off',
+                ),
+            ))
+            ->add('date', DateTimeType::class, array(
+                'label' => 'Skriv tidspunkt og dato',
+                'required' => true,
+            ))
+            ->add('information', TextType::class, array(
+                'label' => 'Skriv mer informasjon om kurset',
+                'required' => true,
+                'attr' => array(
+                    'autocomplete' => 'off',
+                ),
+            ))
+            ->add('save', SubmitType::class, array(
+                'label' => 'Meld på!',
+            ));
+    }
+}
