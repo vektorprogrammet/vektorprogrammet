e10b1133f01006091bb1616648875acf508af6c7
haved <krogstie.havard@gmail.com>
2019-11-13 17:58:23 +0100

    Remove unused import

--- src/AppBundle/Form/Type/AdmissionSubscriberType.php
+++ src/AppBundle/Form/Type/AdmissionSubscriberType.php
@@ -7,7 +7,6 @@ use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\SubmitButton;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class AdmissionSubscriberType extends AbstractType
