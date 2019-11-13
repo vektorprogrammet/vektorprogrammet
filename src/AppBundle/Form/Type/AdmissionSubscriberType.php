12f189cf7f77f5dc9668e771dd9a15d19e0b1195
haved <krogstie.havard@gmail.com>
2019-11-13 17:23:57 +0100

    Remove autocomplete from admission subscriber form

--- src/AppBundle/Form/Type/AdmissionSubscriberType.php
+++ src/AppBundle/Form/Type/AdmissionSubscriberType.php
@@ -5,7 +5,9 @@ namespace AppBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\SubmitButton;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class AdmissionSubscriberType extends AbstractType
@@ -16,7 +18,8 @@ class AdmissionSubscriberType extends AbstractType
             ->add('email', EmailType::class, array(
             'label' => false,
             'attr' => [
-                'placeholder' => 'E-post'
+                'placeholder' => 'E-post',
+                'autocomplete' => 'off'
             ]
             ))
             ->add('infoMeeting', CheckboxType::class, array(
@@ -24,6 +27,12 @@ class AdmissionSubscriberType extends AbstractType
             'required' => false,
             'attr' => [
                 'checked' => true
+            ]
+            ))
+            ->add('submit', SubmitType::class, array(
+            'label' => 'Meld deg på interesseliste',
+            'attr' => [
+                'class' => 'btn btn-success'
             ]
             ));
     }
