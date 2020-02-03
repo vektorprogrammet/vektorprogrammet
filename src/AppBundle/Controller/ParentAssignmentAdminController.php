3a1ac6e923d42441751cae52d3d6b2f2cec1a7e4
alexaoh <alex.andunge@gmail.com>
2020-02-03 17:31:30 +0100

    Fixed all admin-interface-methods
    Need to fix form for assigning parents from admin next

--- src/AppBundle/Controller/ParentAssignmentAdminController.php
+++ src/AppBundle/Controller/ParentAssignmentAdminController.php
@@ -4,6 +4,8 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\ParentAssignment;
 use AppBundle\Entity\ParentCourse;
+use AppBundle\Form\Type\ParentAssignmentType;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class ParentAssignmentAdminController extends BaseController
@@ -11,10 +13,34 @@ class ParentAssignmentAdminController extends BaseController
     public function showAction(ParentCourse $course)
     {
         return $this->render('parents/parent-assignment-admin.html.twig', array(
-            'parentsAssigned' => $course->getAssignedParents()
+            'parentsAssigned' => $course->getAssignedParents(),
+            'courseID' => $course->getId(),
         ));
     }
 
+    public function createAction(Request $request, ParentCourse $course)
+    {
+        $parentAssigned = new ParentAssignment();
+        $form = $this->createForm(ParentAssignmentType::class, $parentAssigned);
+        $form->handleRequest($request);
+
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+
+            $parentAssigned->setCourse($course);
+            $em->persist($parentAssigned);
+            $em->flush();
+
+            $this->addFlash("success", "Foreldren er påmeldt.");
+
+            return $this->redirectToRoute('parent_assignment_admin_show', ['id' => $course->getId()]);
+        }
+
+        return $this->render('parents/parent-assignment.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
     public function deleteAction(ParentAssignment $parentAssignment)
     {
         $parentCourseID = $parentAssignment->getCourse()->getId();
@@ -25,10 +51,7 @@ class ParentAssignmentAdminController extends BaseController
         $this->addFlash("success", "\"".$parentAssignment->getNavn()."\" ble slettet");
 
 
-
-        return $this->redirectToRoute('parent_assignment_admin_show', array(
-            'id' => $parentCourseID
-        ));
+        return $this->redirectToRoute('parent_assignment_admin_show', ['id' => $parentCourseID]);
     }
 
 };
