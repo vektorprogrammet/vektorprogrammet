d8157e775d0e97eb50faef9cfc0f0cb1bebbab2f
alexaoh <alex.andunge@gmail.com>
2020-02-10 15:54:27 +0100

    Check in twig (front end) now works as it should /foreldre

--- src/AppBundle/Controller/ParentsController.php
+++ src/AppBundle/Controller/ParentsController.php
@@ -7,7 +7,7 @@ class ParentsController extends BaseController
     public function showAction()
     {
         $parentCourses = $this->getDoctrine()->getRepository('AppBundle:ParentCourse')->findAllParentCoursesOrderedByDate();
-
+        dump($parentCourses);
         return $this->render('/parents/parents.html.twig', array(
             'parentCourses' => $parentCourses,
         ));
