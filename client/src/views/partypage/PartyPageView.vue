fbad360da225e3e18d5436a723e7ceadba2f2465
Mathias Wahl <ma10asw@gmail.com>
2020-09-01 15:43:48 +0200

    change text to speech url for updated domain (#1350)

--- client/src/views/partypage/PartyPageView.vue
+++ client/src/views/partypage/PartyPageView.vue
@@ -195,7 +195,7 @@
 
                 let firstName = user.firstName;
                 let sound = new Audio(require('../../assets/johncenaintro.mp3'));
-                let sound2 = new Audio('http://159.65.58.116/'+ firstName);
+                let sound2 = new Audio('https://ttsaas.sigtot.com/'+ firstName);
                 let sound3 = new Audio(require('../../assets/johncenaout.mp3'));
 
                 sound.volume = 0.6;
