3e3a150f1ee5f11ed9c45ca5aa03d54ffe943b55
lundli <lundli@pm.me>
2020-11-04 21:22:45 +0100

    Remove unused import iter_for

--- src/AppBundle/DataFixtures/ORM/LoadTodoItemData.php
+++ src/AppBundle/DataFixtures/ORM/LoadTodoItemData.php
@@ -7,7 +7,6 @@ use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use AppBundle\Entity\TodoItem;
-use function GuzzleHttp\Promise\iter_for;
 
 class LoadTodoItemData extends AbstractFixture implements OrderedFixtureInterface
 {
