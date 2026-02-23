8d9d09c5dcde3d3d43b4d2a416b98d91e955a6cc
phibkro <71797726+phibkro@users.noreply.github.com>
2026-02-23 06:18:37 +0100

    Revert "feat: add published and slug filters to Article API"
    This reverts commit 080a3fd77e6143c2bc5661ba4d933278f7837946.

--- src/App/Entity/Article.php
+++ src/App/Entity/Article.php
@@ -5,9 +5,6 @@ namespace App\Entity;
 use DateTime;
 use Doctrine\Common\Collections\Collection;
 use App\Entity\Repository\ArticleRepository;
-use ApiPlatform\Doctrine\Orm\Filter\BooleanFilter;
-use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
-use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
@@ -28,8 +25,6 @@ use Symfony\Component\Validator\Constraints as Assert;
     paginationItemsPerPage: 20,
     normalizationContext: ['groups' => ['article:read']],
 )]
-#[ApiFilter(BooleanFilter::class, properties: ['published', 'sticky'])]
-#[ApiFilter(SearchFilter::class, properties: ['slug' => 'exact'])]
 class Article
 {
     #[ORM\Id]
